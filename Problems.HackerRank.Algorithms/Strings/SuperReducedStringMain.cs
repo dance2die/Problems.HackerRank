d980369eb09873c75cfef37a97e1414dd7625250
dance2die <sungmkim80@gmail.com>
2017-01-12 09:15:29 -0500

    Created "SuperReducedStringMain"

---
+++ Problems.HackerRank.Algorithms/Strings/SuperReducedStringMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    public class Solution
+    {
+        public static void Main(string args)
+        {
+            // Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+
+
+        }
+    }
+}
