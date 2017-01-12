26b889882ae228426c337f247508b916581002b8
dance2die <sungmkim80@gmail.com>
2017-01-12 16:44:34 -0500

    I misunderstood the question. need to get this redone completely.

--- Problems.HackerRank.Algorithms/Strings/SuperReducedStringMain.cs
+++ Problems.HackerRank.Algorithms/Strings/SuperReducedStringMain.cs
@@ -8,11 +8,49 @@ namespace Problems.HackerRank.Algorithms.Strings
 {
     public class Solution
     {
-        public static void Main(string args)
+        public static void Main(string[] args)
         {
-            // Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+            string input = Console.ReadLine();
+            //Console.WriteLine(input);
+            //string input = "aaabccddd";
 
+            string output = input;
+            var index = 0;
+            while (RemoveAdjacentPair(ref output, index))
+            {
+                index++;
+            }
 
+            var result = string.IsNullOrWhiteSpace(output) ? "Empty String" : output;
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Returns true if any adjancet pair has been removed.
+        /// </summary>
+        private static bool RemoveAdjacentPair(ref string input, int index)
+        {
+            List<char> tempResult = new List<char>();
+            for (int i = index; i < input.Length; i++)
+            {
+                var realBound = i + 1;
+                if (realBound < input.Length)
+                {
+                    //tempResult.Add(input[i]);
+                    if (input[i] == input[realBound])
+                    {
+                        tempResult.AddRange(input.Substring(realBound));
+                        input = new string(tempResult.ToArray());
+                        return true;
+                    }
+                }
+                else
+                {
+                    index = 0;
+                }
+            }
+
+            return false;
         }
     }
 }
