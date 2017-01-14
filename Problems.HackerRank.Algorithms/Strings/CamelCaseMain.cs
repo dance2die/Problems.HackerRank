8c126dcd1788fa2d77f0e21ddffaa1c56bde1a58
dance2die <dontbothermewithspam@gmail.com>
2017-01-13 22:32:16 -0500

    All tests passed for CamelCase

---
+++ Problems.HackerRank.Algorithms/Strings/CamelCaseMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// CamelCase
+    /// https://www.hackerrank.com/challenges/camelcase?h_r=next-challenge&h_v=zen
+    /// </summary>
+    public class CamelCaseMain
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            // input = "saveChangesInTheEditor";
+            // output = 5
+
+            // Edge case check.
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            // First letter always starts with lower case
+            int count = 1;
+            foreach (char c in input)
+            {
+                if (char.IsUpper(c))
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
