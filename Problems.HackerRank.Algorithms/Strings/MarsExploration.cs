c59a0f34b10f1ab1484eec8609c58b289b3393c9
dance2die <dontbothermewithspam@gmail.com>
2017-01-13 23:14:06 -0500

    Created "MarsExploration" I will solve this tomorrow... too tired.

---
+++ Problems.HackerRank.Algorithms/Strings/MarsExploration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// Mars Exploration
+    /// https://www.hackerrank.com/challenges/mars-exploration
+    /// </summary>
+    public class MarsExploration
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+        }
+    }
+}
