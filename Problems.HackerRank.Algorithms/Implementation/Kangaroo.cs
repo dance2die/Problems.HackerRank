83ac9d775162f3a92eb506237fe503ff90daf92f
dance2die <sungmkim80@gmail.com>
2017-01-20 15:55:40 -0500

    Created Kangaroo stub

---
+++ Problems.HackerRank.Algorithms/Implementation/Kangaroo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Kangaroo
+    /// https://www.hackerrank.com/challenges/kangaroo
+    /// </summary>
+    public class Kangaroo
+    {
+        public static void Main(string[] args)
+        {
+            string[] tokens_x1 = Console.ReadLine().Split(' ');
+            int x1 = Convert.ToInt32(tokens_x1[0]);
+            int v1 = Convert.ToInt32(tokens_x1[1]);
+            int x2 = Convert.ToInt32(tokens_x1[2]);
+            int v2 = Convert.ToInt32(tokens_x1[3]);
+
+
+        }
+    }
+}
