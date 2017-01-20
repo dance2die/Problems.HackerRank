8cd250ad40c93f5001263edcecd6ca3b4224beea
dance2die <sungmkim80@gmail.com>
2017-01-20 13:23:16 -0500

    "Minimum Distances": initial import

---
+++ Problems.HackerRank.Algorithms/Implementation/MinimumDistances.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Minimum Distances
+    /// https://www.hackerrank.com/challenges/minimum-distances
+    /// </summary>
+    public class MinimumDistances
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] A_temp = Console.ReadLine().Split(' ');
+            int[] A = Array.ConvertAll(A_temp, Int32.Parse);
+        }
+    }
+}
