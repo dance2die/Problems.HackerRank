19368756500916d7610010b6e06b2228b4f67365
dance2die <dontbothermewithspam@gmail.com>
2017-01-21 11:12:58 -0500

    "Insertion Sort - Part2" tests pass!

--- Problems.HackerRank.Algorithms/Sorting/InsertionSortPart2.cs
+++ Problems.HackerRank.Algorithms/Sorting/InsertionSortPart2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problems.HackerRank.Algorithms.Sorting
 {
@@ -7,7 +8,7 @@ namespace Problems.HackerRank.Algorithms.Sorting
     /// https://www.hackerrank.com/challenges/insertionsort2
     /// from https://www.hackerrank.com/challenges/tutorial-intro
     /// </summary>
-    public class InsertionSortPart2 : BaseSorting
+    public class InsertionSortPart2
     {
         public static void Main(string[] args)
         {
@@ -29,14 +30,58 @@ OUTPUT:
 1 3 4 5 6 2 
 1 3 4 5 6 2 
 1 2 3 4 5 6 
- */
+
+INPUT:
+9
+9 8 6 7 3 5 4 1 2
+OUTPUT:
+8 9 6 7 3 5 4 1 2
+6 8 9 7 3 5 4 1 2
+6 7 8 9 3 5 4 1 2
+3 6 7 8 9 5 4 1 2
+3 5 6 7 8 9 4 1 2
+3 4 5 6 7 8 9 1 2
+1 3 4 5 6 7 8 9 2
+1 2 3 4 5 6 7 8 9
+ 
+             */
 
             InsertionSort(a);
         }
 
-        private static void InsertionSort(int[] ar)
+        private static void InsertionSort(int[] a)
         {
-            
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                SortFor(a, i + 1);
+                PrintArray(a);
+            }
+        }
+
+        /// <returns>True if element has been moved</returns>
+        private static bool SortFor(int[] a, int currentIndex)
+        {
+            int value = a[currentIndex];
+
+            bool isMoved = false;
+            for (int i = currentIndex; i >= 1; i--)
+            {
+                if (a[i - 1] > value)
+                {
+                    var temp = a[i - 1];
+                    a[i] = temp;
+                    a[i - 1] = value;
+
+                    isMoved = true;
+                }
+            }
+
+            return isMoved;
+        }
+
+        private static void PrintArray(int[] a)
+        {
+            Console.WriteLine(string.Join(" ", a));
         }
     }
 }
