318d0b5ba40b3e5c2fba4876b9c6900859e8319a
dance2die <dontbothermewithspam@gmail.com>
2017-01-21 11:16:21 -0500

    "Correctness and the Loop Invariant" created a stub

---
+++ Problems.HackerRank.Algorithms/Sorting/CorrectnessAndTheLoopInvariant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    /// <summary>
+    /// Correctness and the Loop Invariant
+    /// https://www.hackerrank.com/challenges/correctness-invariant
+    /// from https://www.hackerrank.com/challenges/tutorial-intro
+    /// </summary>
+    public class CorrectnessAndTheLoopInvariant
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int[] a = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+/*
+INPUT:
+6
+1 4 3 5 6 2
+OUTPUT:
+1 2 3 4 5 6 
+ */
+
+            InsertionSort(a);
+        }
+
+        public static void InsertionSort(int[] a)
+        {
+            var j = 0;
+            for (var i = 1; i < a.Length; i++)
+            {
+                var value = a[i];
+                j = i - 1;
+                while (j > 0 && value < a[j])
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                }
+                a[j + 1] = value;
+            }
+            Console.WriteLine(string.Join(" ", a));
+        }
+    }
+}
