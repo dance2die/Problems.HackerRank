fc4ce161c288fb6589d31929205af696bdeca083
dance2die <dontbothermewithspam@gmail.com>
2017-01-21 10:27:10 -0500

    one test out of 5 fails.

--- Problems.HackerRank.Algorithms/Sorting/InsertionSortPart1.cs
+++ Problems.HackerRank.Algorithms/Sorting/InsertionSortPart1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problems.HackerRank.Algorithms.Sorting
 {
@@ -20,12 +21,58 @@ namespace Problems.HackerRank.Algorithms.Sorting
                 a[i] = Convert.ToInt32(elementsSplit[i]);
             }
 
+/*
+INPUT:
+5
+2 4 6 8 3
+OUTPUT:
+2 4 6 8 8 
+2 4 6 6 8 
+2 4 4 6 8 
+2 3 4 6 8 
+
+INPUT:
+14
+1 3 5 9 13 22 27 35 46 51 55 83 87 23
+OUTPUT:
+1 3 5 9 13 22 27 35 46 51 55 83 87 87
+1 3 5 9 13 22 27 35 46 51 55 83 83 87
+1 3 5 9 13 22 27 35 46 51 55 55 83 87
+1 3 5 9 13 22 27 35 46 51 51 55 83 87
+1 3 5 9 13 22 27 35 46 46 51 55 83 87
+1 3 5 9 13 22 27 35 35 46 51 55 83 87
+1 3 5 9 13 22 27 27 35 46 51 55 83 87
+1 3 5 9 13 22 23 27 35 46 51 55 83 87
+
+ */
+
             InsertionSort(a);
         }
 
         private static void InsertionSort(int[] a)
         {
-            
+            int value = a.Last();
+
+            bool done = false;
+            for (int i = a.Length - 2; i >= 0; i--)
+            {
+                a[i + 1] = a[i];
+
+                if (a[i] <= value)
+                {
+                    a[i + 1] = value;
+                    done = true;
+                }
+
+                PrintArray(a);
+
+                if (done) break;
+            }
+        }
+
+        private static void PrintArray(int[] a)
+        {
+            Console.WriteLine(string.Join(" ", a));
         }
     }
 }
