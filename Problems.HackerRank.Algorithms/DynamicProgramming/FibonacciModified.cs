b5be5d0b49fa58f3f6367f81211a6a93dcced677
dance2die <dontbothermewithspam@gmail.com>
2017-01-21 09:44:43 -0500

    "FibonacciModified" Initial Import

---
+++ Problems.HackerRank.Algorithms/DynamicProgramming/FibonacciModified.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.DynamicProgramming
+{
+    /// <summary>
+    /// Fibonacci Modified
+    /// https://www.hackerrank.com/challenges/fibonacci-modified
+    /// </summary>
+    public class FibonacciModified
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int seed1 = int.Parse(input[0]);
+            int seed2 = int.Parse(input[1]);
+            int n = int.Parse(input[2]);
+            // Input 0 1 5 
+            // output 5
+
+
+        }
+    }
+}
