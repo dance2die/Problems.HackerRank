a9ad8a34c253d433cb5aa28ba18eeae5c3a45401
dance2die <dontbothermewithspam@gmail.com>
2017-01-22 13:18:30 -0500

    Created a stub for "Quicksort 1 - Partition"

---
+++ Problems.HackerRank.Algorithms/Sorting/QuickSort1Partition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    /// <summary>
+    /// Quicksort 1 - Partition
+    /// https://www.hackerrank.com/challenges/quicksort1
+    /// from https://www.hackerrank.com/challenges/tutorial-intro
+    /// </summary>
+    public class QuickSort1Partition
+    {
+        public static void Main(string[] args)
+        {
+            var arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[arraySize];
+            string elements = Console.ReadLine();
+            if (elements != null)
+            {
+                string[] elementsSplit = elements.Split(' ');
+                for (int i = 0; i < arraySize; i++)
+                {
+                    a[i] = Convert.ToInt32(elementsSplit[i]);
+                }
+            }
+
+            Partition(a);
+        }
+
+        private static void Partition(int[] a)
+        {
+            
+        }
+    }
+}
