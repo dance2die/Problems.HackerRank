013ac7ee9fc15227fa834b944f177f44394f6901
dance2die <dontbothermewithspam@gmail.com>
2017-01-22 13:01:26 -0500

    Fixed the issue in "Correctness and the Loop Invariant" by tracking loop invariant that the first element should always be sorted. The problem of the code was that it was begnning from the second element assuming that the first element was sorted.

--- Problems.HackerRank.Algorithms/Sorting/CorrectnessAndTheLoopInvariant.cs
+++ Problems.HackerRank.Algorithms/Sorting/CorrectnessAndTheLoopInvariant.cs
@@ -15,12 +15,20 @@ namespace Problems.HackerRank.Algorithms.Sorting
             Console.ReadLine();
             int[] a = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
 /*
+
 INPUT:
 6
 1 4 3 5 6 2
 OUTPUT:
 1 2 3 4 5 6 
- */
+
+INPUT:
+9
+9 8 6 7 3 5 4 1 2
+OUTPUT:
+1 2 3 4 5 6 7 8 9
+
+*/
 
             InsertionSort(a);
         }
@@ -28,17 +36,18 @@ OUTPUT:
         public static void InsertionSort(int[] a)
         {
             var j = 0;
-            for (var i = 1; i < a.Length; i++)
+            for (var i = 0; i < a.Length; i++)
             {
                 var value = a[i];
                 j = i - 1;
-                while (j > 0 && value < a[j])
+                while (j >= 0 && value < a[j])
                 {
                     a[j + 1] = a[j];
                     j = j - 1;
                 }
                 a[j + 1] = value;
             }
+
             Console.WriteLine(string.Join(" ", a));
         }
     }
