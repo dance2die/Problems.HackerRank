45ca679ad76c8076385fa3b830ea144c26b292f9
dance2die <dontbothermewithspam@gmail.com>
2017-01-22 13:15:30 -0500

    "Running Time of Algorithms" tests pass!

--- Problems.HackerRank.Algorithms/Sorting/RunningTimeOfAlgorithms.cs
+++ Problems.HackerRank.Algorithms/Sorting/RunningTimeOfAlgorithms.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problems.HackerRank.Algorithms.Sorting
 {
@@ -11,6 +12,40 @@ namespace Problems.HackerRank.Algorithms.Sorting
     {
         public static void Main(string[] args)
         {
+            Console.ReadLine();
+            int[] a = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+
+/*
+INPUT:
+5
+2 1 3 1 2
+ 
+OUTPUT:
+4
+*/
+
+            var shiftCount = InsertionSort(a);
+            Console.WriteLine(shiftCount);
+        }
+
+        public static int InsertionSort(int[] a)
+        {
+            var j = 0;
+            int shiftCount = 0;
+            for (var i = 0; i < a.Length; i++)
+            {
+                var value = a[i];
+                j = i - 1;
+                while (j >= 0 && value < a[j])
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                    shiftCount++;
+                }
+                a[j + 1] = value;
+            }
+
+            return shiftCount;
         }
     }
 }
