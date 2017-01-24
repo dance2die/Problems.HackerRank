b2254340d7ffd82f9a8741b70adcbc3ac96b1b6e
dance2die <dontbothermewithspam@gmail.com>
2017-01-23 19:46:44 -0500

    Created a stub for "Quicksort 2 - Sorting"

---
+++ Problems.HackerRank.Algorithms/Sorting/QuickSort2Sorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    public class QuickSort2Sorting
+    {
+        /// <summary>
+        /// Quicksort 2 - Sorting
+        /// https://www.hackerrank.com/challenges/quicksort2
+        /// from https://www.hackerrank.com/challenges/tutorial-intro
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            var arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[arraySize];
+            string elements = Console.ReadLine();
+            string[] elementsSplit = elements.Split(' ');
+            for (int i = 0; i < arraySize; i++)
+            {
+                a[i] = Convert.ToInt32(elementsSplit[i]);
+            }
+
+            QuickSort(a);
+        }
+
+        private static void QuickSort(int[] a)
+        {
+            
+        }
+    }
+}
