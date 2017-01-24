bee4f1cdb062ebcd545d5be853155aa1b52f70a7
dance2die <dontbothermewithspam@gmail.com>
2017-01-23 19:44:41 -0500

    "Quicksort 1 - Partition" tests pass!

--- Problems.HackerRank.Algorithms/Sorting/QuickSort1Partition.cs
+++ Problems.HackerRank.Algorithms/Sorting/QuickSort1Partition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problems.HackerRank.Algorithms.Sorting
 {
@@ -22,13 +23,42 @@ namespace Problems.HackerRank.Algorithms.Sorting
                     a[i] = Convert.ToInt32(elementsSplit[i]);
                 }
             }
+/*
+INPUT:
+5
+4 5 3 7 2 
+
+OUTPUT:
+3 2 4 5 7
+*/
 
             Partition(a);
         }
 
         private static void Partition(int[] a)
         {
-            
+            var p = a[0];
+            int partitionSize = a.Length / 2;
+            var left = new List<int>(partitionSize);
+            var right = new List<int>(partitionSize);
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                int value = a[i];
+
+                if (value < p)
+                    left.Add(value);
+
+                if (value > p)
+                    right.Add(value);
+            }
+
+            Console.WriteLine("{0} {1} {2}", string.Join(" ", left.ToArray()), p, string.Join(" ", right.ToArray()));
+        }
+
+        private static void PrintArray(int[] a)
+        {
+            Console.WriteLine(string.Join(" ", a));
         }
     }
 }
