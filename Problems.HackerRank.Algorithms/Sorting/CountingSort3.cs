5d32c119d85fe7550885f22ffc882ebd7387fa15
dance2die <sungmkim80@gmail.com>
2017-01-25 16:31:24 -0500

    created a stub for "Counting Sort 3"

---
+++ Problems.HackerRank.Algorithms/Sorting/CountingSort3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    /// <summary>
+    /// Counting Sort 3
+    /// https://www.hackerrank.com/challenges/countingsort3
+    /// from https://www.hackerrank.com/challenges/tutorial-intro
+    /// </summary>
+    public class CountingSort3
+    {
+        public static void Main(string[] args)
+        {
+/*
+INPUT:
+10
+4 that
+3 be
+0 to
+1 be
+5 question
+1 or
+2 not
+4 is
+2 to
+4 the
+ 
+OUTPUT:
+1 3 5 6 9 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 
+*/   
+        }
+    }
+}
