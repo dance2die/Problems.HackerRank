e9c210bba2067b50ffc9a50045bfaa1cdedc31c8
dance2die <sungmkim80@gmail.com>
2017-01-27 12:36:17 -0500

    "Closest Numbers" stub.

---
+++ Problems.HackerRank.Algorithms/Sorting/ClosestNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    /// <summary>
+    /// Closest Numbers
+    /// https://www.hackerrank.com/challenges/closest-numbers
+    /// </summary>
+    public class ClosestNumbers
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
