4a7bc27bd870e0559cb33ff2fd876c1e13a9266a
dance2die <dontbothermewithspam@gmail.com>
2017-01-29 14:39:05 -0500

    Lily's homework stub created.

---
+++ Problems.HackerRank.Algorithms/Sorting/LilysHomework.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    /// <summary>
+    /// Lily's Homework
+    /// https://www.hackerrank.com/challenges/lilys-homework
+    /// </summary>
+    public class LilysHomework
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine(); // ignore array size.
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+/*
+INPUT:
+4
+2 5 3 1
+OUTPUT:
+2
+ */
+        }
+    }
+}
