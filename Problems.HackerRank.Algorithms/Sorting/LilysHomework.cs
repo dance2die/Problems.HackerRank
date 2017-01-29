1aa2ec8b7bae69f9002660bfe9ff0bec2731f0b6
dance2die <dontbothermewithspam@gmail.com>
2017-01-29 14:48:18 -0500

    Only 2 out of 11 tests pass!

--- Problems.HackerRank.Algorithms/Sorting/LilysHomework.cs
+++ Problems.HackerRank.Algorithms/Sorting/LilysHomework.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Problems.HackerRank.Algorithms.Sorting
 {
@@ -11,7 +12,7 @@ namespace Problems.HackerRank.Algorithms.Sorting
         public static void Main(string[] args)
         {
             Console.ReadLine(); // ignore array size.
-            int[] values = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 /*
 INPUT:
 4
@@ -19,6 +20,36 @@ INPUT:
 OUTPUT:
 2
  */
+            int swapCount = GetMinimumSwapCountToSort(a);
+            Console.WriteLine(swapCount);
+        }
+
+        private static int GetMinimumSwapCountToSort(int[] a)
+        {
+            var sorted = ((int[]) a.Clone()).ToList();
+            sorted.Sort();
+
+            int swapCount = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                int currentLowest = sorted[i];
+                int currentValue = a[i];
+                if (currentValue > currentLowest)
+                {
+                    int currentLowestValueIndex = Array.IndexOf(a, currentLowest);
+                    Swap(a, i, currentLowestValueIndex);
+                    swapCount++;
+                }
+            }
+
+            return swapCount;
+        }
+
+        private static void Swap(int[] a, int i1, int i2)
+        {
+            var temp = a[i1];
+            a[i1] = a[i2];
+            a[i2] = temp;
         }
     }
 }
