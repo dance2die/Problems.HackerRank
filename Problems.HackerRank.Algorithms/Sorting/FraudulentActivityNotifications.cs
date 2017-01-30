204133d6d034139b3e839b990e81105860f75d0e
dance2die <dontbothermewithspam@gmail.com>
2017-01-30 07:24:32 -0500

    "Fraudulent Activity Notifications" stub.

---
+++ Problems.HackerRank.Algorithms/Sorting/FraudulentActivityNotifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Sorting
+{
+    /// <summary>
+    /// Fraudulent Activity Notifications
+    /// https://www.hackerrank.com/challenges/fraudulent-activity-notifications
+    /// </summary>
+    public class FraudulentActivityNotifications
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
