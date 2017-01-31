73274a701b855e499eedf78e73d4492f531ca583
dance2die <dontbothermewithspam@gmail.com>
2017-01-31 06:38:01 -0500

    "Fraudulent Activity Notifications": parses input.

--- Problems.HackerRank.Algorithms/Sorting/FraudulentActivityNotifications.cs
+++ Problems.HackerRank.Algorithms/Sorting/FraudulentActivityNotifications.cs
@@ -10,6 +10,19 @@ namespace Problems.HackerRank.Algorithms.Sorting
     {
         public static void Main(string[] args)
         {
+            var firstLine = Console.ReadLine().Split(' ');
+            int arraySize = int.Parse(firstLine[0]);
+            int days = int.Parse(firstLine[1]);
+
+            var expenditures = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+
+/*
+INPUT:
+9 5
+2 3 4 2 3 6 8 4 5
+OUTPUT: 2
+*/
         }
     }
 }
