49739c1460ba60ff8a549d6acc6e5f61e594e835
dance2die <dontbothermewithspam@gmail.com>
2017-02-02 21:19:50 -0500

    Lonely Integer stub created.

---
+++ Problems.HackerRank.Algorithms/BitManipulation/LonelyInteger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.BitManipulation
+{
+    /// <summary>
+    /// Lonely Integer
+    /// https://www.hackerrank.com/challenges/lonely-integer
+    /// </summary>
+    public class LonelyInteger
+    {
+        static void Main(String[] args)
+        {
+            int res;
+
+            int _a_size = Convert.ToInt32(Console.ReadLine());
+            int[] _a = new int[_a_size];
+            int _a_item;
+            String move = Console.ReadLine();
+            String[] move_split = move.Split(' ');
+            for (int _a_i = 0; _a_i < move_split.Length; _a_i++)
+            {
+                _a_item = Convert.ToInt32(move_split[_a_i]);
+                _a[_a_i] = _a_item;
+            }
+            res = lonelyinteger(_a);
+            Console.WriteLine(res);
+        }
+
+        static int lonelyinteger(int[] a)
+        {
+            return 0;
+        }
+    }
+}
