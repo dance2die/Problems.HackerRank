b6442c5469d2e2e98ca0a174ea3d028dbbd1201c
dance2die <dontbothermewithspam@gmail.com>
2017-02-05 23:13:45 -0500

    Maximizing XOR stub.

---
+++ Problems.HackerRank.Algorithms/BitManipulation/MaximizingXor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.BitManipulation
+{
+    /// <summary>
+    /// Maximizing XOR
+    /// https://www.hackerrank.com/challenges/maximizing-xor
+    /// </summary>
+    public class MaximizingXor
+    {
+        public static void Main(string[] args)
+        {
+            int res;
+            int _l;
+            _l = Convert.ToInt32(Console.ReadLine());
+
+            int _r;
+            _r = Convert.ToInt32(Console.ReadLine());
+
+            res = MaxXor(_l, _r);
+            Console.WriteLine(res);
+        }
+
+        static int MaxXor(int l, int r)
+        {
+            return -1;
+        }
+    }
+}
