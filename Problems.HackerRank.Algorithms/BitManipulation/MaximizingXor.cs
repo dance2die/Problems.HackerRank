e6ba85de5bc2a5466fe19e7f8c871fbe48088ad9
dance2die <dontbothermewithspam@gmail.com>
2017-02-06 06:38:03 -0500

    Maximizing XOR tests pass!

--- Problems.HackerRank.Algorithms/BitManipulation/MaximizingXor.cs
+++ Problems.HackerRank.Algorithms/BitManipulation/MaximizingXor.cs
@@ -10,20 +10,42 @@ namespace Problems.HackerRank.Algorithms.BitManipulation
     {
         public static void Main(string[] args)
         {
-            int res;
-            int _l;
-            _l = Convert.ToInt32(Console.ReadLine());
+            var left = Convert.ToInt32(Console.ReadLine());
+            var right = Convert.ToInt32(Console.ReadLine());
 
-            int _r;
-            _r = Convert.ToInt32(Console.ReadLine());
+/*
+INPUT:
+10
+15
+OUTPUT: 7
 
-            res = MaxXor(_l, _r);
-            Console.WriteLine(res);
+INPUT:
+5
+6
+OUTPUT: 3
+*/
+
+            var result = MaxXor(left, right);
+            Console.WriteLine(result);
         }
 
-        static int MaxXor(int l, int r)
+        public static int MaxXor(int left, int right)
         {
-            return -1;
+            int max = 0;
+            Func<int, string> toBin = v => Convert.ToString(v, 2);
+
+            for (int i = left; i <= right; i++)
+            {
+                for (int j = left; j <= right; j++)
+                {
+                    var xorValue = i ^ j;
+                    //Console.WriteLine("{0}({1}) ^ {2}({3}) = {4}({5})", i, toBin(i), j, toBin(j), xorValue, toBin(xorValue));
+                    if (xorValue > max)
+                        max = xorValue;
+                }
+            }
+
+            return max;
         }
     }
 }
