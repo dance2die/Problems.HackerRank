2d15d826d6a173b83f0ec1f9618b8a4964e777e3
dance2die <dontbothermewithspam@gmail.com>
2017-02-06 06:47:46 -0500

    Only 8 out of 12 tests pass due to time out.

---
+++ Problems.HackerRank.Algorithms/BitManipulation/SumVsXor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.BitManipulation
+{
+    /// <summary>
+    /// Sum vs XOR
+    /// https://www.hackerrank.com/challenges/sum-vs-xor
+    /// </summary>
+    public class SumVsXor
+    {
+        public static void Main(string[] args)
+        {
+            long number = Convert.ToInt64(Console.ReadLine());
+/*
+INPUT: 5 OUTPUT: 2
+INPUT: 10 OUTPUT: 4
+
+ */
+
+            int result = GetEqualSumAndXorCount(number);
+            Console.WriteLine(result);
+        }
+
+        private static int GetEqualSumAndXorCount(long number)
+        {
+            int count = 0;
+            int mask = 1;
+            for (int i = 0; i <= number; i++)
+            {
+                mask = 1 << i;
+                long sum = number + i;
+                long xor = number ^ i;
+                if (sum == xor)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
