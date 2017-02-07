cdc679eaa53abd159a3d56dbd88f52368a5bbd94
dance2die <sungmkim80@gmail.com>
2017-02-07 12:08:43 -0500

    Two Characters stub

---
+++ Problems.HackerRank.Algorithms/Strings/TwoCharacters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// Two Characters
+    /// https://www.hackerrank.com/challenges/two-characters
+    /// </summary>
+    public class TwoCharacters
+    {
+        public static void Main(String[] args)
+        {
+            int len = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+        }
+    }
+}
