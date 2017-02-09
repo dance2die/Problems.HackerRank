1d329235809c7f6b8e094e6ac1cf8291fd795baa
dance2die <sungmkim80@gmail.com>
2017-02-09 11:25:48 -0500

    Times out for most of the cases.

--- Problems.HackerRank.Algorithms/Strings/TwoCharacters.cs
+++ Problems.HackerRank.Algorithms/Strings/TwoCharacters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Problems.HackerRank.Algorithms.Strings
@@ -30,31 +31,61 @@ OUTPUT: 5
             List<char> alreadyChecked = new List<char>();
 
             int maxCount = 0;
-            foreach (char c in text)
+            foreach (char c1 in text)
             {
-                if (alreadyChecked.Contains(c)) continue;
+                foreach (var c2 in text.Where(c => c != c1))
+                {
+                    if (alreadyChecked.Contains(c1)) continue;
 
-                var alternatingCharacterCount = GetAlternatingCharacterCount(text, c);
-                if (alternatingCharacterCount > maxCount)
-                    maxCount = alternatingCharacterCount;
+                    var alternatingCharacterCount = GetAlternatingCharacterCount(text, c1, c2);
+                    if (alternatingCharacterCount > maxCount)
+                        maxCount = alternatingCharacterCount;
+                }
             }
 
             return maxCount;
         }
 
-        private static int GetAlternatingCharacterCount(string text, char c)
+        private static int GetAlternatingCharacterCount(string text, char c1, char c2)
         {
             int result = int.MinValue;
-            var filteredText = new string(text.Where(c1 => c1 != c).ToArray());
+            var charsToRemove = text.Where(c => c != c1 && c != c2);
 
-            if (IsAlternating(filteredText))
-                return filteredText.Length;
-            return result;;
+            int max = 0;
+            List<char> removedChars = new List<char>();
+            foreach (var removeChar in charsToRemove)
+            {
+                removedChars.Add(removeChar);
+                var filteredText = new string(text.Where(c => removedChars.Contains(c)).ToArray());
+                if (IsAlternating(filteredText) && filteredText.Length > max)
+                    max = filteredText.Length;
+            }
+
+            return max;
         }
 
         private static bool IsAlternating(string text)
         {
-            return false;
+            Debug.Assert(text.Length >= 1);
+            if (text.Length == 1) return true;
+
+            char c1 = text[0];
+            char c2 = text[1];
+            if (c1 == c2) return false;
+
+            for (int i = 2; i < text.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (text[i] != c1) return false;
+                }
+                else
+                {
+                    if (text[i] != c2) return false;
+                }
+            }
+
+            return true;
         }
     }
 }
