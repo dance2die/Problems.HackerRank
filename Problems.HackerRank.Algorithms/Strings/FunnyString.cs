8fa04215450f731626031a29bf8686a20a4eb145
dance2die <sungmkim80@gmail.com>
2017-02-09 12:14:20 -0500

    "Funny String" stub.

---
+++ Problems.HackerRank.Algorithms/Strings/FunnyString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// Funny String
+    /// https://www.hackerrank.com/challenges/funny-string
+    /// </summary>
+    public class FunnyString
+    {
+        public static void Main(string[] args)
+        {
+            int testCaseCount = int.Parse(Console.ReadLine());
+            var testCases = GetTestCases(testCaseCount);
+        }
+
+        private static IEnumerable<string> GetTestCases(int testCaseCount)
+        {
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                yield return Console.ReadLine();
+            }
+        }
+    }
+}
