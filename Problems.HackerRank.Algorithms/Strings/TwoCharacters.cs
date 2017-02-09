896254b70599d15eaeb260222247a0dd104bc083
dance2die <dontbothermewithspam@gmail.com>
2017-02-09 07:38:59 -0500

    checked in to work somewhere else.

--- Problems.HackerRank.Algorithms/Strings/TwoCharacters.cs
+++ Problems.HackerRank.Algorithms/Strings/TwoCharacters.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Problems.HackerRank.Algorithms.Strings
 {
@@ -25,7 +27,34 @@ OUTPUT: 5
 
         private static int GetLongestSequence(string text)
         {
-            return -1;
+            List<char> alreadyChecked = new List<char>();
+
+            int maxCount = 0;
+            foreach (char c in text)
+            {
+                if (alreadyChecked.Contains(c)) continue;
+
+                var alternatingCharacterCount = GetAlternatingCharacterCount(text, c);
+                if (alternatingCharacterCount > maxCount)
+                    maxCount = alternatingCharacterCount;
+            }
+
+            return maxCount;
+        }
+
+        private static int GetAlternatingCharacterCount(string text, char c)
+        {
+            int result = int.MinValue;
+            var filteredText = new string(text.Where(c1 => c1 != c).ToArray());
+
+            if (IsAlternating(filteredText))
+                return filteredText.Length;
+            return result;;
+        }
+
+        private static bool IsAlternating(string text)
+        {
+            return false;
         }
     }
 }
