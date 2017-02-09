a723d3720276bceff7590292d57b96748aaeb446
dance2die <sungmkim80@gmail.com>
2017-02-09 15:30:32 -0500

    Alternating Characters tests pass!

--- Problems.HackerRank.Algorithms/Strings/AlternatingCharacters.cs
+++ Problems.HackerRank.Algorithms/Strings/AlternatingCharacters.cs
@@ -45,21 +45,15 @@ OUTPUT:
         private static int GetDeleteCount(string testCase)
         {
             int count = 0;
-            int length = testCase.Length;
             for (int i = 0; i < testCase.Length - 1; i++)
             {
                 var c1 = testCase[i];
                 var c2 = testCase[i + 1];
                 if (c1 == c2)
                 {
-                    testCase = testCase.Remove(i, 1);
-                    i = -1;
                     count++;
                 }
             }
-
-            if (testCase.Length > 1 && testCase[testCase.Length - 2] == testCase[testCase.Length - 1])
-                count++;
 
             return count;
         }
