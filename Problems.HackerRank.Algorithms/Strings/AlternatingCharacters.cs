74c2ab061862061278b9bc35eec0511417414972
dance2die <sungmkim80@gmail.com>
2017-02-09 15:19:25 -0500

    4 out of 13 cases time out.

--- Problems.HackerRank.Algorithms/Strings/AlternatingCharacters.cs
+++ Problems.HackerRank.Algorithms/Strings/AlternatingCharacters.cs
@@ -17,23 +17,51 @@ namespace Problems.HackerRank.Algorithms.Strings
 /*
 INPUT:
 5
+AAABBB
 AAAA
 BBBBB
 ABABABAB
 BABABA
-AAABBB
 OUTPUT:
+4
 3
 4
 0
 0
-4
+
 */
             PrintTestCases(testCases);
         }
 
         private static void PrintTestCases(List<string> testCases)
         {
+            foreach (var testCase in testCases)
+            {
+                int deleteCount = GetDeleteCount(testCase);
+                Console.WriteLine(deleteCount);
+            }
+        }
+
+        private static int GetDeleteCount(string testCase)
+        {
+            int count = 0;
+            int length = testCase.Length;
+            for (int i = 0; i < testCase.Length - 1; i++)
+            {
+                var c1 = testCase[i];
+                var c2 = testCase[i + 1];
+                if (c1 == c2)
+                {
+                    testCase = testCase.Remove(i, 1);
+                    i = -1;
+                    count++;
+                }
+            }
+
+            if (testCase.Length > 1 && testCase[testCase.Length - 2] == testCase[testCase.Length - 1])
+                count++;
+
+            return count;
         }
 
         private static IEnumerable<string> GetTestCases(int testCaseCount)
