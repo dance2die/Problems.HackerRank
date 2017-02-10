ea8401d168533dfc25da95eb2c51ab05f9ac3223
dance2die <dontbothermewithspam@gmail.com>
2017-02-09 21:23:32 -0500

    "Designer PDF Viewer" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/DesignerPdfViewer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Designer PDF Viewer
+    /// https://www.hackerrank.com/challenges/designer-pdf-viewer
+    /// </summary>
+    public class DesignerPdfViewer
+    {
+        public static void Main(string[] args)
+        {
+            string[] heightText = Console.ReadLine().Split(' ');
+            int[] heights = Array.ConvertAll(heightText, int.Parse);
+            string word = Console.ReadLine();
+/*
+INPUT:
+1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+abc
+OUTPUT: 9
+*/
+            int area = GetArea(heights, word);
+            Console.WriteLine(area);
+        }
+
+        private static int GetArea(int[] heights, string word)
+        {
+            return -1;
+        }
+    }
+}
