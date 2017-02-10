9e733c04dd634a372a231da87a732bea66cefa85
dance2die <sungmkim80@gmail.com>
2017-02-10 15:16:18 -0500

    "Between Two Sets" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/BetweenTwoSets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Between Two Sets
+    /// https://www.hackerrank.com/challenges/between-two-sets
+    /// </summary>
+    public class BetweenTwoSets
+    {
+        public static void Main(string[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int m = Convert.ToInt32(tokens_n[1]);
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, int.Parse);
+            string[] b_temp = Console.ReadLine().Split(' ');
+            int[] b = Array.ConvertAll(b_temp, int.Parse);
+        }
+    }
+}
