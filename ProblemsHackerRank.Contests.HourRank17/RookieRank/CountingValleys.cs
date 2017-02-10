703a55450071fa2abef6af8af64b4c3f56cb8e9e
dance2die <sungmkim80@gmail.com>
2017-02-10 12:14:28 -0500

    "Counting Valleys" is submitted successfully.

---
+++ ProblemsHackerRank.Contests.HourRank17/RookieRank/CountingValleys.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProblemsHackerRank.Contests.RookieRank
+{
+    /// <summary>
+    /// Counting Valleys
+    /// https://www.hackerrank.com/contests/rookierank/challenges/counting-valleys
+    /// </summary>
+    public class CountingValleys
+    {
+        public static void Main()
+        {
+            int stepCount = int.Parse(Console.ReadLine());
+            string steps = Console.ReadLine();
+
+/*
+INPUT:
+8
+UDDDUDUU
+OUTPUT: 1
+*/
+
+            int valleyCount = GetValleyCount(steps);
+            Console.WriteLine(valleyCount);
+        }
+
+        private static int GetValleyCount(string steps)
+        {
+            int valleyCount = 0;
+            int currentLevel = 0;
+            foreach (char step in steps)
+            {
+                int previousLevel = currentLevel;
+
+                if (step == 'U')
+                    currentLevel++;
+                else
+                    currentLevel--;
+
+                if (previousLevel < 0 && currentLevel == 0)
+                    valleyCount++;
+            }
+
+            return valleyCount;
+        }
+    }
+}
