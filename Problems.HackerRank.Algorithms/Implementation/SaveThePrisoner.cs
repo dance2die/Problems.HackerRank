1cb898bf5dec48e6c1ac95f5c592a4aa53ddcfe5
dance2die <dontbothermewithspam@gmail.com>
2017-02-11 10:02:55 -0500

    "Save the Prisoner!" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/SaveThePrisoner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Save the Prisoner!
+    /// https://www.hackerrank.com/challenges/save-the-prisoner
+    /// </summary>
+    public class SaveThePrisoner
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
