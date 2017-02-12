7e75375f278149e987da9e36924b64746f545ecb
dance2die <dontbothermewithspam@gmail.com>
2017-02-12 09:42:30 -0500

    cheated from discussion.

--- Problems.HackerRank.Algorithms/Implementation/SaveThePrisoner.cs
+++ Problems.HackerRank.Algorithms/Implementation/SaveThePrisoner.cs
@@ -1,9 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/*
+INPUT:
+1 
+5 2 1
+OUTPUT: 2
+
+INPUT:
+2
+6 8 5
+5 2 1
+OUTPUT:
+6
+2
+
+INPUT:
+3
+352926151 380324688 94730870
+94431605 679262176 5284458
+208526924 756265725 150817879
+OUTPUT:
+122129406
+23525398
+72975907
+ */
 namespace Problems.HackerRank.Algorithms.Implementation
 {
     /// <summary>
@@ -14,7 +36,51 @@ namespace Problems.HackerRank.Algorithms.Implementation
     {
         public static void Main(string[] args)
         {
+            int testCaseCount = int.Parse(Console.ReadLine());
+            List<PrisonerTestCase> testCases = GetPrisonerTestCases(testCaseCount).ToList();
 
+            PrintWarnIds(testCases);
+        }
+
+        private static void PrintWarnId(PrisonerTestCase testCase)
+        {
+            int result = (testCase.PrisonerId + testCase.SweetCount - 1) % testCase.PrisonerCount;
+            if (result == 0)
+                result = testCase.PrisonerCount;
+            Console.WriteLine(result);
+        }
+
+        private static void PrintWarnIds(List<PrisonerTestCase> testCases)
+        {
+            foreach (PrisonerTestCase testCase in testCases)
+            {
+                PrintWarnId(testCase);
+            }
+        }
+
+        private static IEnumerable<PrisonerTestCase> GetPrisonerTestCases(int testCaseCount)
+        {
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                var line = Console.ReadLine();
+                var splitLine = line.Split(' ');
+                var numbers = Array.ConvertAll(splitLine, int.Parse);
+                yield return new PrisonerTestCase(numbers[0], numbers[1], numbers[2]);
+            }
+        }
+    }
+
+    public class PrisonerTestCase
+    {
+        public int PrisonerCount { get; set; }
+        public int SweetCount { get; set; }
+        public int PrisonerId { get; set; }
+
+        public PrisonerTestCase(int prisonerCount, int sweetCount, int prisonerId)
+        {
+            PrisonerCount = prisonerCount;
+            SweetCount = sweetCount;
+            PrisonerId = prisonerId;
         }
     }
 }
