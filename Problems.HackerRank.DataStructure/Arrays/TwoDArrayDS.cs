b2975cc32ae9786f3fe20b8a5c775bcf71bca017
dance2die <dontbothermewithspam@gmail.com>
2017-02-13 07:13:02 -0500

    all tests pass

--- Problems.HackerRank.DataStructure/Arrays/TwoDArrayDS.cs
+++ Problems.HackerRank.DataStructure/Arrays/TwoDArrayDS.cs
@@ -9,6 +9,15 @@ INPUT:
 0 0 0 2 0 0
 0 0 1 2 4 0
 OUTPUT: 19
+
+INPUT:
+-1 -1 0 -9 -2 -2
+-2 -1 -6 -8 -2 -5
+-1 -1 -1 -2 -3 -4
+-1 -9 -2 -4 -4 -5
+-7 -3 -3 -2 -9 -9
+-1 -3 -1 -2 -4 -5
+OUTPUT: -6
  */
 namespace Problems.HackerRank.DataStructure.Arrays
 {
@@ -35,7 +44,7 @@ namespace Problems.HackerRank.DataStructure.Arrays
 
         private static int GetMaxSum(int[][] a)
         {
-            int maxSum = 0;
+            int maxSum = int.MinValue;
             for (int i = 0; i < ARRAY_SIZE - 2; i++)
             {
                 for (int j = 0; j < ARRAY_SIZE - 2; j++)
