87295c966777e07a947a3aea59245532408dd2c0
dance2die <dontbothermewithspam@gmail.com>
2017-02-13 07:15:47 -0500

    Dynamic Array stub.

---
+++ Problems.HackerRank.DataStructure/Arrays/DynamicArray.cs
@@ -0,0 +1,27 @@
+﻿/*
+INPUT: 
+2 5
+1 0 5
+1 1 7
+1 0 3
+2 1 0
+2 1 1
+OUTPUT:
+7
+3
+
+ */
+namespace Problems.HackerRank.DataStructure.Arrays
+{
+    /// <summary>
+    /// Dynamic Array
+    /// https://www.hackerrank.com/challenges/dynamic-array
+    /// </summary>
+    public class DynamicArray
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
