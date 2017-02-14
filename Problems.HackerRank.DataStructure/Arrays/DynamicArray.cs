9025cbdfb1864d6e2819562e53e2000bc886df02
dance2die <sungmkim80@gmail.com>
2017-02-14 11:06:30 -0500

    sample passes but not test cases.

--- Problems.HackerRank.DataStructure/Arrays/DynamicArray.cs
+++ Problems.HackerRank.DataStructure/Arrays/DynamicArray.cs
@@ -10,6 +10,129 @@ OUTPUT:
 7
 3
 
+INPUT:
+100 100
+1 345255357 205970905
+1 570256166 75865401
+1 94777800 645102173
+1 227496730 16649450
+1 82987157 486734305
+1 917920354 757848208
+1 61379773 817694251
+1 330547128 112869154
+1 328743001 855677723
+1 407951306 669798718
+1 21506172 676980108
+1 49911390 342109400
+1 980306253 305632965
+2 736380701 402184046
+2 798108301 416334323
+1 254839279 1370035
+1 109701362 2800586
+1 374257441 165208824
+1 624259835 477431250
+1 629066664 454406245
+1 135821145 763845832
+1 480298791 138234911
+1 553575409 835718837
+1 13022848 624652920
+1 974893519 882630870
+1 137832930 216177975
+1 453349691 969255659
+1 138396076 91038209
+1 699822497 941751038
+1 116800806 64071662
+1 815273934 8835809
+1 658534867 657771609
+1 183760807 179377441
+1 93984556 636425656
+1 231506463 836238924
+1 214074594 709571211
+1 649641434 509698468
+2 523656673 709717705
+2 261443586 330808140
+1 75924023 449768243
+1 849537714 354568873
+2 641743742 124196560
+1 19829224 995759639
+1 244389335 108315212
+1 877758467 421383626
+1 573278148 474192994
+2 561031511 448889978
+1 773294404 980994962
+2 33088709 716646168
+1 760927835 441983538
+1 273540687 783321829
+1 5933845 384358662
+1 543628702 372160176
+2 136400466 910559291
+2 546568738 393221347
+1 812227065 694221123
+1 311065574 103905420
+2 571344361 185289590
+1 99638520 173318136
+1 854060080 407068012
+2 980658213 778573744
+2 412539660 476853104
+1 530145366 36493537
+1 604875364 100141497
+2 650812104 64817757
+1 141060009 766603553
+1 598398952 418245941
+1 262344011 431865586
+2 56413893 546484833
+1 400736735 673588153
+1 642955232 803851098
+1 917782037 935143105
+1 658284524 745224102
+1 103202288 501551287
+1 162144918 12888783
+1 16486753 67467208
+1 671120703 941541277
+1 47399694 77707668
+1 122011395 946116527
+1 924363862 886726236
+2 657761235 540240467
+1 203175991 279882007
+2 304620923 162838413
+1 440453449 117964712
+2 941868853 887850334
+1 293198923 466812643
+1 461688477 532794906
+1 315016797 733095902
+1 265008751 913972757
+1 887405255 139170948
+2 754223230 426836947
+1 945967814 852589735
+1 168866283 309720694
+1 373861145 94596540
+2 984122495 20702849
+2 233835636 180460242
+1 172787631 643823473
+1 273611372 616819555
+1 196104599 690080895
+1 527554061 434103342
+OUTPUT:
+855677723
+75865401
+763845832
+75865401
+709571211
+645102173
+112869154
+763845832
+449768243
+757848208
+91038209
+205970905
+783321829
+372160176
+384358662
+67467208
+935143105
+384358662
+309720694
+138234911
  */
 
 using System;
@@ -27,11 +150,39 @@ namespace Problems.HackerRank.DataStructure.Arrays
         public static void Main(string[] args)
         {
             var firstLineInputs = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-            int sequenceCount = firstLineInputs[0];
+            //int sequenceCount = firstLineInputs[0];
             int queryCount = firstLineInputs[1];
 
             List<Query> queries = GetQueries(queryCount).ToList();
-            Console.WriteLine(queries);
+            PrintQueries(queries);
+        }
+
+        private static void PrintQueries(List<Query> queries)
+        {
+            List<List<int>> seq = new List<List<int>>(2)
+            {
+                new List<int>(),
+                new List<int>()
+            };
+            int lastAnswer = 0;
+
+            foreach (Query query in queries)
+            {
+                int seqIndex = GetSequenceIndex(query.X, lastAnswer);
+
+                if (query.Type == 2)
+                {
+                    lastAnswer = seq[seqIndex].Last();
+                    Console.WriteLine(lastAnswer);
+                }
+
+                seq[seqIndex].Add(query.Y);
+            }
+        }
+
+        private static int GetSequenceIndex(int x, int lastAnswer)
+        {
+            return (x ^ lastAnswer) % 2;
         }
 
         private static IEnumerable<Query> GetQueries(int queryCount)
@@ -39,20 +190,22 @@ namespace Problems.HackerRank.DataStructure.Arrays
             for (int i = 0; i < queryCount; i++)
             {
                 var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-                yield return new Query(line[0], new List<int>(line.Skip(1).ToArray()));
+                yield return new Query(line[0], line[1], line[2]);
             }
         }
     }
 
     public class Query
     {
-        public int QueryType { get; set; }
-        public List<int> Sequences { get; set; }
+        public int Type { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
-        public Query(int queryType, List<int> sequences)
+        public Query(int type, int x, int y)
         {
-            QueryType = queryType;
-            Sequences = sequences;
+            Type = type;
+            X = x;
+            Y = y;
         }
     }
 }
