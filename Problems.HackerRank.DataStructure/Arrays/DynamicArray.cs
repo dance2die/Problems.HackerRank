a6cca724f49a3d3202b5b4e9e0cedad1660f7253
dance2die <sungmkim80@gmail.com>
2017-02-14 10:16:56 -0500

    "Dynamic Array" input parsing complete.

--- Problems.HackerRank.DataStructure/Arrays/DynamicArray.cs
+++ Problems.HackerRank.DataStructure/Arrays/DynamicArray.cs
@@ -11,6 +11,11 @@ OUTPUT:
 3
 
  */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Problems.HackerRank.DataStructure.Arrays
 {
     /// <summary>
@@ -21,7 +26,33 @@ namespace Problems.HackerRank.DataStructure.Arrays
     {
         public static void Main(string[] args)
         {
+            var firstLineInputs = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int sequenceCount = firstLineInputs[0];
+            int queryCount = firstLineInputs[1];
 
+            List<Query> queries = GetQueries(queryCount).ToList();
+            Console.WriteLine(queries);
+        }
+
+        private static IEnumerable<Query> GetQueries(int queryCount)
+        {
+            for (int i = 0; i < queryCount; i++)
+            {
+                var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                yield return new Query(line[0], new List<int>(line.Skip(1).ToArray()));
+            }
+        }
+    }
+
+    public class Query
+    {
+        public int QueryType { get; set; }
+        public List<int> Sequences { get; set; }
+
+        public Query(int queryType, List<int> sequences)
+        {
+            QueryType = queryType;
+            Sequences = sequences;
         }
     }
 }
