b8421d1ddddee8426aabe4c341b69832e2e91218
dance2die <sungmkim80@gmail.com>
2017-02-14 15:15:23 -0500

    Left Rotation stub

---
+++ Problems.HackerRank.DataStructure/Arrays/LeftRotation.cs
@@ -0,0 +1,14 @@
+﻿namespace Problems.HackerRank.DataStructure.Arrays
+{
+    /// <summary>
+    /// Left Rotation
+    /// https://www.hackerrank.com/challenges/array-left-rotation
+    /// </summary>
+    public class LeftRotation
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
