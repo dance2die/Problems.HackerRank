b85a566759e8eeb24f6ebb08707f92d1ac621f85
dance2die <sungmkim80@gmail.com>
2017-02-15 10:23:42 -0500

    Left Rotation stub.

--- Problems.HackerRank.DataStructure/Arrays/LeftRotation.cs
+++ Problems.HackerRank.DataStructure/Arrays/LeftRotation.cs
@@ -1,4 +1,14 @@
-﻿namespace Problems.HackerRank.DataStructure.Arrays
+﻿/*
+INPUT:
+5 4
+1 2 3 4 5
+OUTPUT:
+5 1 2 3 4
+ */
+
+using System;
+
+namespace Problems.HackerRank.DataStructure.Arrays
 {
     /// <summary>
     /// Left Rotation
@@ -8,7 +18,18 @@
     {
         public static void Main()
         {
-            
+            var firstLine = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int size = firstLine[0];
+            int rotationCount = firstLine[1];
+            var a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            string output = GetLeftRotationText(a, rotationCount);
+            Console.WriteLine(output);
+        }
+
+        private static string GetLeftRotationText(int[] a, int rotationCount)
+        {
+            return "";
         }
     }
 }
