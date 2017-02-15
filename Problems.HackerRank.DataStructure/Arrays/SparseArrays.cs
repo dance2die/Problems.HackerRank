2eba199923bdc90bae4100bc3bcbd2afa9c6eb9b
dance2die <sungmkim80@gmail.com>
2017-02-15 12:29:47 -0500

    "Sparse Arrays" parses input correctly.

--- Problems.HackerRank.DataStructure/Arrays/SparseArrays.cs
+++ Problems.HackerRank.DataStructure/Arrays/SparseArrays.cs
@@ -1,4 +1,25 @@
-﻿namespace Problems.HackerRank.DataStructure.Arrays
+﻿/*
+INPUT:
+4
+aba
+baba
+aba
+xzxb
+3
+aba
+xzxb
+ab
+OUTPUT:
+2
+1
+0
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems.HackerRank.DataStructure.Arrays
 {
     /// <summary>
     /// Sparse Arrays
@@ -8,7 +29,18 @@
     {
         public static void Main()
         {
-            
+            int textCount = int.Parse(Console.ReadLine());
+            List<string> texts = GetTexts(textCount).ToList();
+            int queryCount = int.Parse(Console.ReadLine());
+            List<string> queries = GetTexts(queryCount).ToList();
+        }
+
+        private static IEnumerable<string> GetTexts(int textCount)
+        {
+            for (int i = 0; i < textCount; i++)
+            {
+                yield return Console.ReadLine();
+            }
         }
     }
 }
