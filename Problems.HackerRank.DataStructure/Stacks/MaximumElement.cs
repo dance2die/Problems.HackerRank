29664d682b27a4206c049a9958b4f68ea5808f44
dance2die <dontbothermewithspam@gmail.com>
2017-02-16 07:43:26 -0500

    "Maximum Element" parses input correctly.

--- Problems.HackerRank.DataStructure/Stacks/MaximumElement.cs
+++ Problems.HackerRank.DataStructure/Stacks/MaximumElement.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿/*
+INPUT:
+10
+1 97
+2
+1 20
+2
+1 26
+1 20
+2
+3
+1 91
+3
+OUTPUT:
+26
+91
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Problems.HackerRank.DataStructure.Stacks
 {
@@ -14,7 +30,39 @@ namespace Problems.HackerRank.DataStructure.Stacks
     {
         public static void Main(string[] args)
         {
-
+            int queryCount = int.Parse(Console.ReadLine());
+            List<Query> queries = GetQueries(queryCount).ToList();
         }
+
+        private static IEnumerable<Query> GetQueries(int queryCount)
+        {
+            for (int i = 0; i < queryCount; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                if (line.Length == 1)
+                    yield return new Query((QueryType)int.Parse(line[0]), null);
+                else
+                    yield return new Query((QueryType)int.Parse(line[0]), Int32.Parse(line[1]));
+            }
+        }
+    }
+
+    public class Query
+    {
+        public QueryType Type { get; set; }
+        public int? Value { get; set; }
+
+        public Query(QueryType type, int? value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+    public enum QueryType
+    {
+        Push = 1,
+        Pop = 2,
+        Print = 3
     }
 }
