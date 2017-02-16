fdc3d8dffd5c2e979ceb1ce3244ddfdbe61cd15e
dance2die <dontbothermewithspam@gmail.com>
2017-02-16 07:24:38 -0500

    "Maximum Element" stub.

---
+++ Problems.HackerRank.DataStructure/Stacks/MaximumElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HackerRank.DataStructure.Stacks
+{
+    /// <summary>
+    /// Maximum Element
+    /// https://www.hackerrank.com/challenges/maximum-element
+    /// </summary>
+    public class MaximumElement
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
