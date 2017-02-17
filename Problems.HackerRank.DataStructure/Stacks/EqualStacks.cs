983bd788e0fca98f9193e70c36ba2bca6ed2c3f6
dance2die <dontbothermewithspam@gmail.com>
2017-02-17 07:49:30 -0500

    not passing any test.

--- Problems.HackerRank.DataStructure/Stacks/EqualStacks.cs
+++ Problems.HackerRank.DataStructure/Stacks/EqualStacks.cs
@@ -29,28 +29,28 @@ namespace Problems.HackerRank.DataStructure.Stacks
     {
         public static void Main()
         {
-            var lines = File.ReadAllLines(@".\Stacks\EqualStacks_TestCase06.txt");
-            string[] tokens_n1 = lines[0].Split(' ');
-            int n1 = Convert.ToInt32(tokens_n1[0]);
-            int n2 = Convert.ToInt32(tokens_n1[1]);
-            int n3 = Convert.ToInt32(tokens_n1[2]);
-            string[] h1_temp = lines[1].Split(' ');
-            int[] h1 = Array.ConvertAll(h1_temp, int.Parse);
-            string[] h2_temp = lines[2].Split(' ');
-            int[] h2 = Array.ConvertAll(h2_temp, int.Parse);
-            string[] h3_temp = lines[3].Split(' ');
-            int[] h3 = Array.ConvertAll(h3_temp, int.Parse);
-
-            //string[] tokens_n1 = Console.ReadLine().Split(' ');
+            //var lines = File.ReadAllLines(@".\Stacks\EqualStacks_TestCase30.txt");
+            //string[] tokens_n1 = lines[0].Split(' ');
             //int n1 = Convert.ToInt32(tokens_n1[0]);
             //int n2 = Convert.ToInt32(tokens_n1[1]);
             //int n3 = Convert.ToInt32(tokens_n1[2]);
-            //string[] h1_temp = Console.ReadLine().Split(' ');
+            //string[] h1_temp = lines[1].Split(' ');
             //int[] h1 = Array.ConvertAll(h1_temp, int.Parse);
-            //string[] h2_temp = Console.ReadLine().Split(' ');
+            //string[] h2_temp = lines[2].Split(' ');
             //int[] h2 = Array.ConvertAll(h2_temp, int.Parse);
-            //string[] h3_temp = Console.ReadLine().Split(' ');
+            //string[] h3_temp = lines[3].Split(' ');
             //int[] h3 = Array.ConvertAll(h3_temp, int.Parse);
+
+            string[] tokens_n1 = Console.ReadLine().Split(' ');
+            int n1 = Convert.ToInt32(tokens_n1[0]);
+            int n2 = Convert.ToInt32(tokens_n1[1]);
+            int n3 = Convert.ToInt32(tokens_n1[2]);
+            string[] h1_temp = Console.ReadLine().Split(' ');
+            int[] h1 = Array.ConvertAll(h1_temp, int.Parse);
+            string[] h2_temp = Console.ReadLine().Split(' ');
+            int[] h2 = Array.ConvertAll(h2_temp, int.Parse);
+            string[] h3_temp = Console.ReadLine().Split(' ');
+            int[] h3 = Array.ConvertAll(h3_temp, int.Parse);
 
             Stopwatch watch = new Stopwatch();
             watch.Start();
@@ -67,13 +67,6 @@ namespace Problems.HackerRank.DataStructure.Stacks
             var h1Accumulated = Accumulate(h1);
             var h2Accumulated = Accumulate(h2);
             var h3Accumulated = Accumulate(h3);
-
-            //for (int i = h1Accumulated.Count - 1; i >= 0; i--)
-            //{
-            //    int value = h1Accumulated[i];
-            //    if (h2Accumulated.Contains(value) && h3Accumulated.Contains(value))
-            //        return value;
-            //}
 
             var sorted = h1Accumulated.ToArray().Reverse();
             foreach (long value in sorted)
