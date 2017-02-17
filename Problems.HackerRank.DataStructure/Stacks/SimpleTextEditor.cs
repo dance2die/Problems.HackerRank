53cbf037290ce61b6999715b9b0477aa79585c33
dance2die <sungmkim80@gmail.com>
2017-02-17 15:36:38 -0500

    "Simple Text Editor" stub.

---
+++ Problems.HackerRank.DataStructure/Stacks/SimpleTextEditor.cs
@@ -0,0 +1,30 @@
+﻿/*
+INPUT:
+8
+1 abc
+3 3
+2 3
+1 xy
+3 2
+4 
+4 
+3 1
+OUTPUT:
+c
+y
+a
+ */
+namespace Problems.HackerRank.DataStructure.Stacks
+{
+    /// <summary>
+    /// Simple Text Editor
+    /// https://www.hackerrank.com/challenges/simple-text-editor
+    /// </summary>
+    public class SimpleTextEditor
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
