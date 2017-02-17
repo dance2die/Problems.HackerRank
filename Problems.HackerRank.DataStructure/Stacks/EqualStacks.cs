3a7f75601896965e96a8b9976281d633853355ee
dance2die <dontbothermewithspam@gmail.com>
2017-02-17 06:59:22 -0500

    still not passing. Replacing array.contains with hashset.contains for O(1) performance in next version.

--- Problems.HackerRank.DataStructure/Stacks/EqualStacks.cs
+++ Problems.HackerRank.DataStructure/Stacks/EqualStacks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -27,31 +28,37 @@ namespace Problems.HackerRank.DataStructure.Stacks
     {
         public static void Main()
         {
-            //var lines = File.ReadAllLines(@".\Stacks\EqualStacks_TestCase04.txt");
-            //string[] tokens_n1 = lines[0].Split(' ');
-            //int n1 = Convert.ToInt32(tokens_n1[0]);
-            //int n2 = Convert.ToInt32(tokens_n1[1]);
-            //int n3 = Convert.ToInt32(tokens_n1[2]);
-            //string[] h1_temp = lines[1].Split(' ');
-            //int[] h1 = Array.ConvertAll(h1_temp, int.Parse);
-            //string[] h2_temp = lines[2].Split(' ');
-            //int[] h2 = Array.ConvertAll(h2_temp, int.Parse);
-            //string[] h3_temp = lines[3].Split(' ');
-            //int[] h3 = Array.ConvertAll(h3_temp, int.Parse);
-
-            string[] tokens_n1 = Console.ReadLine().Split(' ');
+            var lines = File.ReadAllLines(@".\Stacks\EqualStacks_TestCase06.txt");
+            string[] tokens_n1 = lines[0].Split(' ');
             int n1 = Convert.ToInt32(tokens_n1[0]);
             int n2 = Convert.ToInt32(tokens_n1[1]);
             int n3 = Convert.ToInt32(tokens_n1[2]);
-            string[] h1_temp = Console.ReadLine().Split(' ');
+            string[] h1_temp = lines[1].Split(' ');
             int[] h1 = Array.ConvertAll(h1_temp, int.Parse);
-            string[] h2_temp = Console.ReadLine().Split(' ');
+            string[] h2_temp = lines[2].Split(' ');
             int[] h2 = Array.ConvertAll(h2_temp, int.Parse);
-            string[] h3_temp = Console.ReadLine().Split(' ');
+            string[] h3_temp = lines[3].Split(' ');
             int[] h3 = Array.ConvertAll(h3_temp, int.Parse);
+
+            //string[] tokens_n1 = Console.ReadLine().Split(' ');
+            //int n1 = Convert.ToInt32(tokens_n1[0]);
+            //int n2 = Convert.ToInt32(tokens_n1[1]);
+            //int n3 = Convert.ToInt32(tokens_n1[2]);
+            //string[] h1_temp = Console.ReadLine().Split(' ');
+            //int[] h1 = Array.ConvertAll(h1_temp, int.Parse);
+            //string[] h2_temp = Console.ReadLine().Split(' ');
+            //int[] h2 = Array.ConvertAll(h2_temp, int.Parse);
+            //string[] h3_temp = Console.ReadLine().Split(' ');
+            //int[] h3 = Array.ConvertAll(h3_temp, int.Parse);
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
 
             int commonMaximumHeight = GetCommonMaximumHeight(h1, h2, h3);
             Console.WriteLine(commonMaximumHeight);
+
+            watch.Stop();
+            Console.WriteLine("ellapsed: {0}", watch.Elapsed);
         }
 
         private static int GetCommonMaximumHeight(int[] h1, int[] h2, int[] h3)
@@ -59,13 +66,6 @@ namespace Problems.HackerRank.DataStructure.Stacks
             int[] h1Accumulated = Accumulate(h1);
             int[] h2Accumulated = Accumulate(h2);
             int[] h3Accumulated = Accumulate(h3);
-
-            //var reversed = h1Accumulated.Reverse();
-            //foreach (int value in reversed)
-            //{
-            //    if (h2Accumulated.Contains(value) && h3Accumulated.Contains(value))
-            //        return value;
-            //}
 
             for (int i = h1Accumulated.Length - 1; i >= 0; i--)
             {
