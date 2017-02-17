5d5af8e32bdcb236fb5e824dcf652310849688e4
dance2die <sungmkim80@gmail.com>
2017-02-17 13:19:53 -0500

    "Balanced Brackets" submitted successfully.

---
+++ Problems.HackerRank.DataStructure/Stacks/BalancedBrackets.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+INPUT:
+3
+{[()]}
+{[(])}
+{{[[(())]]}}
+OUTPUT:
+YES
+NO
+YES
+
+INPUT:
+6
+}][}}(}][))]
+[](){()}
+()
+({}([][]))[]()
+{)[](}]}]}))}(())(
+([[)
+OUTPUT:
+NO
+YES
+YES
+YES
+NO
+NO
+ */
+namespace Problems.HackerRank.DataStructure.Stacks
+{
+    /// <summary>
+    /// Balanced Brackets
+    /// https://www.hackerrank.com/challenges/balanced-brackets
+    /// </summary>
+    public class BalancedBrackets
+    {
+        public static void Main()
+        {
+            int caseCount = Convert.ToInt32(Console.ReadLine());
+            List<string> testCases = GetTestCases(caseCount).ToList();
+
+            PrintMatchingBrackets(testCases);
+        }
+
+        private static void PrintMatchingBrackets(List<string> testCases)
+        {
+            foreach (string testCase in testCases)
+            {
+                Console.WriteLine(HasMatchingBrackets(testCase) ? "YES" : "NO");
+            }
+        }
+
+        private static bool HasMatchingBrackets(string testCase)
+        {
+            // Matching brackets should have even number of brackets.
+            Func<string, bool> hasOddLength = text => text.Length % 2 == 1;
+            if (hasOddLength(testCase)) return false;
+
+            var stackSize = testCase.Length / 2;
+            Stack<char> stack = new Stack<char>(stackSize);
+            var openingBracketMap = GetOpeningBracketMap();
+            var closingBracketMap = GetClosingBracketMap();
+
+            Func<char, bool> isOpeningBracket = c => openingBracketMap.Keys.Contains(c);
+            Func<char, bool> isClosingBracket = c => closingBracketMap.Keys.Contains(c);
+            Func<char, bool> isLastOneInStackMatching = c => stack.Peek() == closingBracketMap[c];
+
+            foreach (char c in testCase)
+            {
+                if (isOpeningBracket(c))
+                    stack.Push(c);
+
+                if (stack.Count == 0 && isClosingBracket(c))
+                    return false;
+
+                if (isClosingBracket(c) && isLastOneInStackMatching(c))
+                    stack.Pop();
+            }
+
+            return stack.Count == 0;
+        }
+
+
+        private static Dictionary<char, char> GetOpeningBracketMap()
+        {
+            return new Dictionary<char, char>
+            {
+                { '{', '}' },
+                { '[', ']' },
+                { '(', ')' },
+            };
+        }
+        private static Dictionary<char, char> GetClosingBracketMap()
+        {
+            return new Dictionary<char, char>
+            {
+                { '}', '{' },
+                { ']', '[' },
+                { ')', '(' },
+            };
+        }
+
+        private static IEnumerable<string> GetTestCases(int caseCount)
+        {
+            for (int i = 0; i < caseCount; i++)
+            {
+                yield return Console.ReadLine();
+            }
+        }
+    }
+}
