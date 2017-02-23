fe533ce5de4953bf0c3d4cfc99d49879137e61ef
dance2die <sungmkim80@gmail.com>
2017-02-23 16:36:15 -0500

    "Palindrome Index" stub.

---
+++ Problems.HackerRank.Algorithms/Strings/PalindromeIndex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+INPUT:
+3
+aaab
+baa
+aaa
+OUTPUT:
+3
+0
+-1
+ */
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// Palindrome Index
+    /// https://www.hackerrank.com/challenges/palindrome-index
+    /// </summary>
+    public class PalindromeIndex
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
