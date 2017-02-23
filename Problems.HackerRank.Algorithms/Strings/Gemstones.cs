0279a380070dab8af4e1deadd1f878986245904c
dance2die <sungmkim80@gmail.com>
2017-02-23 13:23:49 -0500

    "Gemstones" stub

---
+++ Problems.HackerRank.Algorithms/Strings/Gemstones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// Gemstones 
+    /// https://www.hackerrank.com/challenges/gem-stones
+    /// </summary>
+    public class Gemstones
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
