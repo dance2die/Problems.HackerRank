ea3ce7628701ef08181682b0f76b11ed289fe241
dance2die <dontbothermewithspam@gmail.com>
2017-02-24 07:03:59 -0500

    9 out of 15 times out.

--- Problems.HackerRank.Algorithms/Strings/PalindromeIndex.cs
+++ Problems.HackerRank.Algorithms/Strings/PalindromeIndex.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
 INPUT:
@@ -25,7 +23,45 @@ namespace Problems.HackerRank.Algorithms.Strings
     {
         public static void Main()
         {
-            
+            List<string> testCases = Enumerable.Range(0, int.Parse(Console.ReadLine()))
+                .Select(_ => Console.ReadLine()).ToList();
+
+            PrintPalindromeIndexes(testCases);
+        }
+
+        private static void PrintPalindromeIndexes(List<string> testCases)
+        {
+            foreach (string testCase in testCases)
+            {
+                Console.WriteLine(GetRemovedIndex(testCase));
+            }
+        }
+
+        /// <remarks>
+        /// Partially copied from TheLoveLetterMystery
+        /// </remarks>
+        private static int GetRemovedIndex(string testCase)
+        {
+            if (IsPalindrome(testCase)) return -1;
+
+            for (int i = 0; i < testCase.Length; i++)
+            {
+                var tempCase = testCase.Remove(i, 1);
+                if (IsPalindrome(tempCase))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        private static bool IsPalindrome(string testCase)
+        {
+            int middleIndex = testCase.Length / 2;
+            Func<string, bool> hasEvenLength = str => str.Length % 2 == 0;
+            var left = testCase.Substring(0, middleIndex);
+            var right = new string(testCase.Substring(hasEvenLength(testCase) ? middleIndex : middleIndex + 1).Reverse().ToArray());
+
+            return left == right;
         }
     }
 }
