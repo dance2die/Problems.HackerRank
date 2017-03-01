3e53a677caff4633b5116ee83c7b5cc3aeab25ac
dance2die <dontbothermewithspam@gmail.com>
2017-03-01 07:37:25 -0500

    "Game of Thrones - I" stub.

---
+++ Problems.HackerRank.Algorithms/Strings/GameOfThrones1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// Game of Thrones - I
+    /// https://www.hackerrank.com/challenges/game-of-thrones
+    /// </summary>
+    public class GameOfThrones1
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
