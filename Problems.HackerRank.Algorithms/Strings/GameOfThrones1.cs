26d1d3643518112c142cf59587f36fba5019f425
dance2die <dontbothermewithspam@gmail.com>
2017-03-02 07:07:07 -0500

    "Game of Thrones - I": created failing GetHasPalindromePermutation

--- Problems.HackerRank.Algorithms/Strings/GameOfThrones1.cs
+++ Problems.HackerRank.Algorithms/Strings/GameOfThrones1.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+INPUT: aaabbbb
+OUTPUT: YES
+
+INPUT: cdefghmnopqrstuvw
+OUTPUT: NO
+
+INPUT: cdcdcdcdeeeef
+OUTPUT: YES
+ */
 namespace Problems.HackerRank.Algorithms.Strings
 {
     /// <summary>
@@ -14,7 +24,19 @@ namespace Problems.HackerRank.Algorithms.Strings
     {
         public static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            string hasPalindromePermutation = GetHasPalindromePermutation(input);
+            Console.WriteLine(hasPalindromePermutation);
+        }
+
+        private static string GetHasPalindromePermutation(string input)
+        {
+            // if all of charactersets have even number then "YES".
+            // if all BUT one charactersets have even number then "YES".
+            // else "NO"
+
+            return "NO";
         }
     }
 }
