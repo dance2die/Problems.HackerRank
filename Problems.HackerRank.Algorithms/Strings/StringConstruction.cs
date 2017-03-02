968707375f4b99c7f4c9a3e114864a9cfd9fc89c
dance2die <dontbothermewithspam@gmail.com>
2017-03-02 07:19:29 -0500

    "String Construction" stub.

---
+++ Problems.HackerRank.Algorithms/Strings/StringConstruction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Strings
+{
+    /// <summary>
+    /// String Construction
+    /// https://www.hackerrank.com/challenges/string-construction
+    /// </summary>
+    public class StringConstruction
+    {
+        public static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+            }
+        }
+    }
+}
