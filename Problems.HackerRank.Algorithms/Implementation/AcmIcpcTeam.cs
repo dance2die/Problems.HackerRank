691930ce08ff27b009af08dbf436827549bf9d26
dance2die <sungmkim80@gmail.com>
2017-03-03 11:07:14 -0500

    GetMaximumTopics works but not GetMaximumTeams

--- Problems.HackerRank.Algorithms/Implementation/AcmIcpcTeam.cs
+++ Problems.HackerRank.Algorithms/Implementation/AcmIcpcTeam.cs
@@ -1,5 +1,17 @@
 ﻿using System;
 
+/*
+INPUT:
+4 5
+10101
+11100
+11010
+00101
+OUTPUT:
+5
+2
+
+*/
 namespace Problems.HackerRank.Algorithms.Implementation
 {
     /// <summary>
@@ -10,14 +22,79 @@ namespace Problems.HackerRank.Algorithms.Implementation
     {
         public static void Main()
         {
-            string[] tokens_n = Console.ReadLine().Split(' ');
-            int n = Convert.ToInt32(tokens_n[0]);
-            int m = Convert.ToInt32(tokens_n[1]);
-            string[] topic = new string[n];
-            for (int topic_i = 0; topic_i < n; topic_i++)
+            string[] tokens = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens[0]);
+            int m = Convert.ToInt32(tokens[1]);
+            string[] topics = new string[n];
+            for (int i = 0; i < n; i++)
             {
-                topic[topic_i] = Console.ReadLine();
+                topics[i] = Console.ReadLine();
             }
+
+            Tuple<int, int> maximums = GetMaximums(topics);
+            Console.WriteLine($"{maximums.Item1}\n{maximums.Item2}");
+        }
+
+        private static Tuple<int, int> GetMaximums(string[] topics)
+        {
+            int maximumTopics = GetMaximumTopics(topics);
+            int maximumTeams = GetMaximumTeams(topics, maximumTopics);
+
+            return Tuple.Create(maximumTopics, maximumTeams);
+        }
+
+        private static int GetMaximumTopics(string[] topics)
+        {
+            int maximum = 0;
+            
+            for (int i = 0; i < topics.Length - 2; i++)
+            {
+                var topic = topics[i];
+
+                for (int j = 1; j < topic.Length - 1; j++)
+                {
+                    int currMaximum = 0;
+                    var nextTopic = topics[j];
+                    for (int k = 0; k < topic.Length; k++)
+                    {
+                        int left = int.Parse(topic[k].ToString());
+                        int right = int.Parse(nextTopic[k].ToString());
+                        if ((left | right) == 1)
+                            currMaximum++;
+                    }
+
+                    if (currMaximum > maximum)
+                        maximum = currMaximum;
+                }
+
+            }
+
+            return maximum;
+        }
+
+        private static int GetMaximumTeams(string[] topics, int maximumTopics)
+        {
+            int count = 0;
+            for (int i = 0; i < topics.Length - 2; i++)
+            {
+                var topic = topics[i];
+
+                for (int j = 1; j < topic.Length - 1; j++)
+                {
+                    var nextTopic = topics[i + 1];
+                    int currMaximum = 0;
+                    for (int k = 0; k < topic.Length; k++)
+                    {
+                        if (topic[k] == nextTopic[k] && topic[k] == 1)
+                            currMaximum++;
+                    }
+
+                    if (currMaximum == maximumTopics)
+                        count++;
+                }
+            }
+
+            return count;
         }
     }
 }
