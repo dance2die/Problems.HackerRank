5cf38682ec23ccad47562ef97ca58e8450ae557b
dance2die <sungmkim80@gmail.com>
2017-03-08 12:27:57 -0500

    "Sequence Equation" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/SequenceEquation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+INPUT:
+3
+2 3 1
+OUTPUT:
+2
+3
+1
+ */
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Sequence Equation
+    /// https://www.hackerrank.com/challenges/permutation-equation
+    /// </summary>
+    public class SequenceEquation
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
