e2145da0e22d559beec9d0a6d0ba91dc505c51ca
dance2die <dontbothermewithspam@gmail.com>
2017-03-08 21:29:27 -0500

    "Jumping on the Clouds: Revisited" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/JumpingOnTheCloudsRevisited.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Jumping on the Clouds: Revisited
+    /// https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited
+    /// </summary>
+    public class JumpingOnTheCloudsRevisited
+    {
+        public static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens[0]);
+            int k = Convert.ToInt32(tokens[1]);
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(temp, int.Parse);
+        }
+    }
+}
