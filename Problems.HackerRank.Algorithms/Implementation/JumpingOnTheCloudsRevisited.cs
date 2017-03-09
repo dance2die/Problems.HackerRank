268cd4a8eed888e0ae8e6b3b85a72dff9f8dd02b
dance2die <sungmkim80@gmail.com>
2017-03-09 13:19:59 -0500

    7 out of 8 passes...

--- Problems.HackerRank.Algorithms/Implementation/JumpingOnTheCloudsRevisited.cs
+++ Problems.HackerRank.Algorithms/Implementation/JumpingOnTheCloudsRevisited.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+/*
+INPUT:
+8 2
+0 0 1 0 0 1 1 0
+OUTPUT: 92
+ */
 namespace Problems.HackerRank.Algorithms.Implementation
 {
     /// <summary>
@@ -15,6 +21,28 @@ namespace Problems.HackerRank.Algorithms.Implementation
             int k = Convert.ToInt32(tokens[1]);
             string[] temp = Console.ReadLine().Split(' ');
             int[] c = Array.ConvertAll(temp, int.Parse);
+
+            int energy = GetEnergyAfterJumping(c, k);
+            Console.WriteLine(energy);
+        }
+
+        private static int GetEnergyAfterJumping(int[] a, int k)
+        {
+            int energy = 100;
+            int n = a.Length;
+
+            Func<int, bool> isThunderCloud = val => val == 1;
+
+            for (int i = 0; i < energy; i+=k, energy--)
+            {
+                if (isThunderCloud(a[i]))
+                    energy -= 2;
+
+                if (i > 0 && (i + k) % n == 0)
+                    return --energy;
+            }
+
+            return energy;
         }
     }
 }
