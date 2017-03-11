2a794dbe12f8c5aed821dbf5184b11d708d33a69
dance2die <dontbothermewithspam@gmail.com>
2017-03-11 09:44:18 -0500

    "Cut the sticks" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/CutTheSticks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Cut the sticks
+    /// https://www.hackerrank.com/challenges/cut-the-sticks
+    /// </summary>
+    public class CutTheSticks
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] tempA = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(tempA, int.Parse);
+        }
+    }
+}
