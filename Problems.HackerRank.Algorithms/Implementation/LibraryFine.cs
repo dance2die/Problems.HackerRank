d178a73529e0d7f19ad9486301524d91ba9dbfd8
dance2die <dontbothermewithspam@gmail.com>
2017-03-11 09:25:17 -0500

    "Library Fine" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/LibraryFine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Library Fine
+    /// https://www.hackerrank.com/challenges/library-fine
+    /// </summary>
+    public class LibraryFine
+    {
+        public static void Main(string[] args)
+        {
+            string[] tokens_d1 = Console.ReadLine().Split(' ');
+            int d1 = Convert.ToInt32(tokens_d1[0]);
+            int m1 = Convert.ToInt32(tokens_d1[1]);
+            int y1 = Convert.ToInt32(tokens_d1[2]);
+            string[] tokens_d2 = Console.ReadLine().Split(' ');
+            int d2 = Convert.ToInt32(tokens_d2[0]);
+            int m2 = Convert.ToInt32(tokens_d2[1]);
+            int y2 = Convert.ToInt32(tokens_d2[2]);
+
+        }
+    }
+}
