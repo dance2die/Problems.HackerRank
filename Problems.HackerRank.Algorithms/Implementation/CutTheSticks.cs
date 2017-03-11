948489fa0a527ca53c3362bf3948e8b3d691ab8d
dance2die <dontbothermewithspam@gmail.com>
2017-03-11 10:02:14 -0500

    "Cut the sticks" submitted successfully on first try.

--- Problems.HackerRank.Algorithms/Implementation/CutTheSticks.cs
+++ Problems.HackerRank.Algorithms/Implementation/CutTheSticks.cs
@@ -1,5 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+/*
+INPUT:
+6
+5 4 4 2 2 8
+OUTPUT:
+6
+4
+2
+1
+
+INPUT:
+8
+1 2 3 4 3 3 2 1
+OUTPUT:
+8
+6
+4
+1
+ */
 namespace Problems.HackerRank.Algorithms.Implementation
 {
     /// <summary>
@@ -10,9 +31,33 @@ namespace Problems.HackerRank.Algorithms.Implementation
     {
         public static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
+            int stickCount = Convert.ToInt32(Console.ReadLine());
             string[] tempA = Console.ReadLine().Split(' ');
-            int[] a = Array.ConvertAll(tempA, int.Parse);
+            List<int> sticks = Array.ConvertAll(tempA, int.Parse).ToList();
+
+            List<int> sticksCut = GetSticksCut(sticks).ToList();
+            sticksCut.ForEach(Console.WriteLine);
+        }
+
+        private static IEnumerable<int> GetSticksCut(List<int> sticks)
+        {
+            while (sticks.Count >= 1)
+            {
+                if (sticks.Count == 0)
+                    yield break;
+                yield return sticks.Count;
+
+                int minSize = sticks.Min();
+                for (int i = 0; i < sticks.Count; i++)
+                {
+                    sticks[i] -= minSize;
+                    if (sticks[i] <= 0)
+                    {
+                        sticks.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
         }
     }
 }
