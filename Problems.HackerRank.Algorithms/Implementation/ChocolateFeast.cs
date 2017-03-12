ee8f0ae0b0381c6357e7267244cd603fc9cff288
dance2die <dontbothermewithspam@gmail.com>
2017-03-12 07:31:40 -0400

    "Chocolate Feast" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/ChocolateFeast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Chocolate Feast
+    /// https://www.hackerrank.com/challenges/chocolate-feast?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+    /// </summary>
+    public class ChocolateFeast
+    {
+        public static void Main()
+        {
+            int testCaseCount = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(tokens[0]);
+                int c = Convert.ToInt32(tokens[1]);
+                int m = Convert.ToInt32(tokens[2]);
+            }
+        }
+    }
+}
