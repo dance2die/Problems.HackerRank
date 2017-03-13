d9d1725b8cf66d46cb1464b37c9e9e1523ef1415
dance2die <sungmkim80@gmail.com>
2017-03-13 09:47:54 -0400

    added an example inputs.

--- ProblemsHackerRank.Contests.HourRank17/Week of Code 30/CandyReplenishingRobot.cs
+++ ProblemsHackerRank.Contests.HourRank17/Week of Code 30/CandyReplenishingRobot.cs
@@ -1,9 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/*
+INPUT:
+8 3
+5 2 4
+OUTPUT: 5
+ 
+INPUT:
+8 4
+3 1 7 5
+OUTPUT: 11
+ */
 namespace ProblemsHackerRank.Contests.Week_of_Code_30
 {
     /// <summary>
@@ -12,13 +19,15 @@ namespace ProblemsHackerRank.Contests.Week_of_Code_30
     /// </summary>
     public class CandyReplenishingRobot
     {
+        private const int THREASHOLD = 5;
+
         public static void Main()
         {
             string[] tokens_n = Console.ReadLine().Split(' ');
             int n = Convert.ToInt32(tokens_n[0]);
             int t = Convert.ToInt32(tokens_n[1]);
-            string[] c_temp = Console.ReadLine().Split(' ');
-            int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(temp, int.Parse);
         }
     }
 }
