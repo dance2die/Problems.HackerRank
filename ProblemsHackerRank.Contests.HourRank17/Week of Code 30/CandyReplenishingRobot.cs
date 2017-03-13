643fba393a34a661d69e55e8a6e579f460fc9b41
dance2die <sungmkim80@gmail.com>
2017-03-13 09:40:27 -0400

    "Candy Replenishing Robot" stub.

---
+++ ProblemsHackerRank.Contests.HourRank17/Week of Code 30/CandyReplenishingRobot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsHackerRank.Contests.Week_of_Code_30
+{
+    /// <summary>
+    /// Candy Replenishing Robot
+    /// https://www.hackerrank.com/contests/w30/challenges/candy-replenishing-robot
+    /// </summary>
+    public class CandyReplenishingRobot
+    {
+        public static void Main()
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int t = Convert.ToInt32(tokens_n[1]);
+            string[] c_temp = Console.ReadLine().Split(' ');
+            int[] c = Array.ConvertAll(c_temp, Int32.Parse);
+        }
+    }
+}
