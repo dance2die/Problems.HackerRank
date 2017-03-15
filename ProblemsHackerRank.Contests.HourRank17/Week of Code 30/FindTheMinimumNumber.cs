457a318d152736217aec5ad00c5bb4d1e22b4677
dance2die <dontbothermewithspam@gmail.com>
2017-03-15 06:45:50 -0400

    "Melodious password" (Medium) stub.

---
+++ ProblemsHackerRank.Contests.HourRank17/Week of Code 30/FindTheMinimumNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+INPUT: 2
+OUTPUT: min(int, int)
+
+INPUT: 4
+OUTPUT: min(int, min(int, min(int, int)))
+ */
+namespace ProblemsHackerRank.Contests.Week_of_Code_30
+{
+    /// <summary>
+    /// Find the Minimum Number
+    /// https://www.hackerrank.com/contests/w30/challenges/find-the-minimum-number
+    /// </summary>
+    public class FindTheMinimumNumber
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string result = GetMinText(n);
+            Console.WriteLine(result);
+        }
+
+        private static string GetMinText(int n)
+        {
+            const string minimum = "min(int, int)";
+            if (n <= 2) return minimum;
+
+            const string prefix = "min(int, ";
+            const string postfix = ")";
+
+            string buffer = minimum;
+            for (int i = 0 ; i < n - 2; i++)
+            {
+                buffer = $"{prefix}{buffer}{postfix}";
+            }
+
+            return buffer;
+        }
+    }
+}
---
+++ ProblemsHackerRank.Contests.HourRank17/Week of Code 30/MelodiousPassword.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+INPUT: 1
+OUTPUT:
+w
+t
+v
+g
+l
+o
+h
+i
+x
+q
+j
+r
+k
+p
+a
+m
+d
+e
+z
+c
+n
+u
+b
+f
+s
+ */
+namespace ProblemsHackerRank.Contests.Week_of_Code_30
+{
+    /// <summary>
+    /// Melodious password
+    /// https://www.hackerrank.com/contests/w30/challenges/melodious-password
+    /// </summary>
+    public class MelodiousPassword
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
