1b636c43172acb0f81f87613968c26b822c64909
dance2die <sungmkim80@gmail.com>
2017-03-17 12:59:06 -0400

    "Modified Kaprekar Numbers" submitted successfully on third try.

--- Problems.HackerRank.Algorithms/Implementation/ModifiedKaprekarNumbers.cs
+++ Problems.HackerRank.Algorithms/Implementation/ModifiedKaprekarNumbers.cs
@@ -7,6 +7,16 @@ INPUT:
 1
 100
 OUTPUT: 1 9 45 55 99
+
+INPUT:
+400
+700
+OUTPUT: INVALID RANGE
+
+INPUT:
+1
+99999
+OUTPUT: 1 9 45 55 99 297 703 999 2223 2728 4950 5050 7272 7777 9999 17344 22222 77778 82656 95121 99999
  */
 namespace Problems.HackerRank.Algorithms.Implementation
 {
@@ -22,7 +32,11 @@ namespace Problems.HackerRank.Algorithms.Implementation
             int q = int.Parse(Console.ReadLine());
 
             List<string> kaprekarNumbers = GetKaprekarNumbersBetween(p, q).ToList();
-            Console.WriteLine(string.Join(" ", kaprekarNumbers));
+
+            if (kaprekarNumbers.Count == 0)
+                Console.WriteLine("INVALID RANGE");
+            else
+                Console.WriteLine(string.Join(" ", kaprekarNumbers));
         }
 
         private static IEnumerable<string> GetKaprekarNumbersBetween(int from, int to)
@@ -39,19 +53,19 @@ namespace Problems.HackerRank.Algorithms.Implementation
             if (number == 1) return true;
             if (2 <= number && number <= 8) return false;
 
-            int squared = number * number;
-            Tuple<int, int> separated = GetLeftRightNumbers(squared);
+            double squared = Math.Pow(number, 2);
+            Tuple<long, long> separated = GetLeftRightNumbers(squared);
 
             return separated.Item1 + separated.Item2 == number;
         }
 
-        private static Tuple<int, int> GetLeftRightNumbers(int number)
+        private static Tuple<long, long> GetLeftRightNumbers(double number)
         {
             string numberText = number.ToString();
             int middleIndex = numberText.Length / 2;
 
-            int left = int.Parse(numberText.Substring(0, middleIndex));
-            int right = int.Parse(numberText.Substring(middleIndex));
+            long left = long.Parse(numberText.Substring(0, middleIndex));
+            long right = long.Parse(numberText.Substring(middleIndex));
 
             return Tuple.Create(left, right);
         }
