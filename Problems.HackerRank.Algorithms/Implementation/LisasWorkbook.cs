a32f253b73c65315454211e85720e36fd475f604
dance2die <dontbothermewithspam@gmail.com>
2017-03-19 13:13:25 -0400

    6 out of 11 cases fail.

---
+++ Problems.HackerRank.Algorithms/Implementation/LisasWorkbook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+INPUT:
+5 3  
+4 2 6 1 10
+OUTPUT: 4
+ */
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Lisa's Workbook
+    /// https://www.hackerrank.com/challenges/lisa-workbook
+    /// </summary>
+    public class LisasWorkbook
+    {
+        public static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split(' ');
+            int chapterCount = int.Parse(firstLine[0]);
+            int maxProblemsPerPage = int.Parse(firstLine[1]);
+            List<int> problemCounts = Console.ReadLine().Split(' ').Select(val => int.Parse(val)).ToList();
+
+            int specialPageCount = GetSpecialPageCount(problemCounts, maxProblemsPerPage);
+            Console.WriteLine(specialPageCount);
+        }
+
+        private static int GetSpecialPageCount(List<int> problemCounts, int maxProblemsPerPage)
+        {
+            int specialPageCount = 0;
+            int page = 1;
+
+            foreach (int problemCount in problemCounts)
+            {
+                for (int i = 1; i <= problemCount; i++)
+                {
+                    if (page == i)
+                        specialPageCount++;
+                }
+                page++;
+            }
+
+            return specialPageCount;
+        }
+    }
+}
