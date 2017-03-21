943b8dbcf3ccd8e6beffdfd2e6ff085a0e8c7582
dance2die <dontbothermewithspam@gmail.com>
2017-03-20 22:00:18 -0400

    "Fair Rations" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/FairRations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Fair Rations
+    /// https://www.hackerrank.com/challenges/fair-rations
+    /// </summary>
+    public class FairRations
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            string[] B_temp = Console.ReadLine().Split(' ');
+            int[] B = Array.ConvertAll(B_temp, Int32.Parse);
+        }
+    }
+}
