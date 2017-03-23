a4ec5e1593ca33769e497d74b2e3fda2966bfb6e
dance2die <sungmkim80@gmail.com>
2017-03-23 13:02:51 -0400

    bad algorithm.

--- Problems.HackerRank.Algorithms/Implementation/ManasaAndStones.cs
+++ Problems.HackerRank.Algorithms/Implementation/ManasaAndStones.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /*
 INPUT:
@@ -36,7 +34,34 @@ namespace Problems.HackerRank.Algorithms.Implementation
 
         private static IEnumerable<string> GetLastStoneList(List<ManasaStone> manasaStones)
         {
-            yield break;
+            foreach (ManasaStone manasaStone in manasaStones)
+            {
+                List<int> stones = GetLastStones(manasaStone).OrderBy(val => val).Distinct().ToList();
+                yield return string.Join(" ", stones.Select(val => val.ToString()).ToArray());
+            }
+        }
+
+        private static List<int> GetLastStones(ManasaStone manasaStone)
+        {
+            int accum = 0;
+            var diff1 = GetLastStones(accum, manasaStone.StoneCount, manasaStone.Diff1).ToList();
+            var diff2 = GetLastStones(accum, manasaStone.StoneCount, manasaStone.Diff2).ToList();
+            diff1.AddRange(diff2);
+            return diff1;
+        }
+
+        private static IEnumerable<int> GetLastStones(int accum, int stoneCount, int diff)
+        {
+            if (stoneCount <= 0)
+            {
+                yield return accum;
+            }
+            else
+            {
+                var diffSum = GetLastStones(accum + diff, stoneCount - 1, diff).ToList();
+                foreach (int sum in diffSum)
+                    yield return sum;
+            }
         }
 
         private static IEnumerable<ManasaStone> GetManasaStones(int testCaseCount)
