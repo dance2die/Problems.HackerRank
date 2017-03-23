4cc5039758bc32a0a5aa084d5bbc29bdf62b8da5
dance2die <dontbothermewithspam@gmail.com>
2017-03-23 07:00:12 -0400

    "Cavity Map" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/CavityMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/*
+INPUT: sample case #1
+4
+1112
+1912
+1892
+1234
+OUTPUT:
+1112
+1X12
+18X2
+1234
+ */
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Cavity Map
+    /// https://www.hackerrank.com/challenges/cavity-map
+    /// </summary>
+    public class CavityMap
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] grid = new string[n];
+            for (int grid_i = 0; grid_i < n; grid_i++)
+            {
+                grid[grid_i] = Console.ReadLine();
+            }
+        }
+    }
+}
