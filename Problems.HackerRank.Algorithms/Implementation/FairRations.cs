71b1f21738ac56521740db13858ddeaed8a77d0a
dance2die <dontbothermewithspam@gmail.com>
2017-03-23 06:49:54 -0400

    "Fair Rations" submitted successfully. Half cheated by checking out the "NO" condition.

--- Problems.HackerRank.Algorithms/Implementation/FairRations.cs
+++ Problems.HackerRank.Algorithms/Implementation/FairRations.cs
@@ -1,5 +1,17 @@
 ﻿using System;
+using System.Linq;
 
+/*
+INPUT: sample case #1
+5
+2 3 4 5 6
+OUTPUT: 4
+
+INPUT: sample case #2
+2
+1 2
+OUTPUT: NO
+ */
 namespace Problems.HackerRank.Algorithms.Implementation
 {
     /// <summary>
@@ -13,6 +25,65 @@ namespace Problems.HackerRank.Algorithms.Implementation
             int N = Convert.ToInt32(Console.ReadLine());
             string[] B_temp = Console.ReadLine().Split(' ');
             int[] B = Array.ConvertAll(B_temp, Int32.Parse);
+
+            PrintSteps(B);
+        }
+
+        private static void PrintSteps(int[] a)
+        {
+            if (CannotBeDone(a))
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                int steps = GetSteps(a);
+                Console.WriteLine(steps);
+            }
+        }
+
+        private static int GetSteps(int[] a)
+        {
+            int steps = 0;
+
+            while (!IsEveryElementEven(a))
+            {
+                for (int i = 0; i < a.Length - 1; i++)
+                {
+                    if (IsOdd(a[i]))
+                    {
+                        a[i]++;
+                        a[i + 1]++;
+
+                        steps += 2;
+                        break;
+                    }
+                }
+            }
+
+            return steps;
+        }
+
+        private static bool IsEveryElementEven(int[] a)
+        {
+            foreach (int i in a)
+            {
+                if (IsOdd(i)) return false;
+            }
+            return true;
+        }
+
+        private static bool IsOdd(int val)
+        {
+            return val % 2 == 1;
+        }
+
+        /// <summary>
+        /// If a sum is odd, then it cannot be done.
+        /// </summary>
+        private static bool CannotBeDone(int[] a)
+        {
+            return a.Sum() % 2 == 1;
         }
     }
 }
