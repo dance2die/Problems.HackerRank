6520d68e471b50ddf1cd5fcca6cda4fdaacd731e
dance2die <sungmkim80@gmail.com>
2017-03-28 09:14:35 -0400

    "Strange Counter" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/StrangeCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Strange Counter
+    /// https://www.hackerrank.com/challenges/strange-code
+    /// </summary>
+    public class StrangeCounter
+    {
+        public static void Main()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            long counter = GetCounter(t);
+            Console.WriteLine(counter);
+        }
+
+        private static long GetCounter(int t)
+        {
+            
+        }
+    }
+}
