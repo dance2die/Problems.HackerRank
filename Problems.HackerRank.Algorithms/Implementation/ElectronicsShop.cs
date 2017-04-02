428b1885f8aeda8e1fb658e9b041b5e59982687b
dance2die <dontbothermewithspam@gmail.com>
2017-04-02 07:35:09 -0400

    "Electronics Shop" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/ElectronicsShop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Electronics Shop
+    /// https://www.hackerrank.com/challenges/electronics-shop
+    /// </summary>
+    public class ElectronicsShop
+    {
+        public static void Main()
+        {
+            string[] tokens_s = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(tokens_s[0]);
+            int n = Convert.ToInt32(tokens_s[1]);
+            int m = Convert.ToInt32(tokens_s[2]);
+            string[] keyboards_temp = Console.ReadLine().Split(' ');
+            int[] keyboards = Array.ConvertAll(keyboards_temp, Int32.Parse);
+            string[] pendrives_temp = Console.ReadLine().Split(' ');
+            int[] pendrives = Array.ConvertAll(pendrives_temp, Int32.Parse);
+        }
+    }
+}
---
+++ Problems.HackerRank.Algorithms/Implementation/StrangeCounter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+/*
+INPUT: 4 OUTPUT 6
+INPUT: 7 OUTPUT 3
+INPUT: 52 OUTPUT 42
+INPUT: 93 OUTPUT 1
+INPUT: 1000000000000 OUTPUT: 649267441662
+ */
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Strange Counter
+    /// https://www.hackerrank.com/challenges/strange-code
+    /// </summary>
+    public class StrangeCounter
+    {
+        private const int INCREASE_AMOUNT = 3;
+
+        public static void Main()
+        {
+            long t = Convert.ToInt64(Console.ReadLine());
+            long counter = GetCounter(t);
+            Console.WriteLine(counter);
+        }
+
+        private static long GetCounter(long t)
+        {
+            long prevLeftStart = 0;
+            long prevLeftEnd = 0;
+            long prevRightStart = 0;
+
+            for (int i = 1; i <= t; i++)
+            {
+                long leftStart = prevLeftStart + GetLeftStart(i);
+                long leftEnd = prevLeftEnd + GetLeftEnd(i);
+
+                if (leftStart <= t && t <= leftEnd)
+                {
+                    long offset = t - leftStart;
+                    long rightStart = GetRightStart(i);
+
+                    return rightStart - offset;
+                }
+
+                prevLeftStart = leftStart;
+                prevLeftEnd = leftEnd;
+            }
+
+            return -1;
+        }
+
+        private static long GetLeftStart(long t)
+        {
+            if (t == 1) return 1;
+            return INCREASE_AMOUNT * (long)Math.Pow(2, t - 2);
+        }
+
+        private static long GetLeftEnd(long t)
+        {
+            if (t == 1) return INCREASE_AMOUNT;
+            return INCREASE_AMOUNT * (long) Math.Pow(2, t - 1);
+        }
+
+        private static long GetRightStart(long t)
+        {
+            if (t == 1) return INCREASE_AMOUNT;
+            return INCREASE_AMOUNT * (long) Math.Pow(2, t - 1);
+        }
+    }
+}
