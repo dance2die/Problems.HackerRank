f4731cfe2b364e26b31fb1c746237ee00a355f3e
dance2die <dontbothermewithspam@gmail.com>
2017-04-05 19:59:12 -0400

    "Picking Numbers" submitted successfully on first try.

---
+++ Problems.HackerRank.Algorithms/Implementation/PickingNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+/*
+INPUT:
+6
+4 6 5 3 3 1
+OUTPUT: 3
+
+INPUT:
+6
+1 2 2 3 1 2
+OUTPUT: 5
+ */
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Picking Numbers
+    /// https://www.hackerrank.com/challenges/picking-numbers
+    /// </summary>
+    public class PickingNumbers
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(temp, int.Parse);
+
+            int differenceCount = GetDifferenceCount2(a);
+            Console.WriteLine(differenceCount);
+        }
+
+        private static int GetDifferenceCount2(int[] a)
+        {
+            int maxCount = 0;
+
+            foreach (int currentValue in a)
+            {
+                var rest = a.Where(val => val != currentValue);
+                var higher = rest.Where(val => 0 <= val - currentValue && val - currentValue <= 1);
+                var lower = rest.Where(val => 0 <= currentValue - val && currentValue - val <= 1);
+                var same = a.Where(val => val == currentValue);
+
+                var currentCount = Math.Max(lower.Count(), higher.Count()) + same.Count();
+                if (currentCount > maxCount)
+                    maxCount = currentCount;
+            }
+
+            return maxCount;
+        }
+    }
+}
