6d3ab7d8c32f1e5a079e6a9579fb55da6825891a
dance2die <dontbothermewithspam@gmail.com>
2017-04-07 06:59:42 -0400

    "Apple and Orange" stub.

---
+++ Problems.HackerRank.Algorithms/Implementation/AppleaNDOrange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Problems.HackerRank.Algorithms.Implementation
+{
+    /// <summary>
+    /// Apple and Orange
+    /// https://www.hackerrank.com/challenges/apple-and-orange
+    /// </summary>
+    public class AppleAndOrange
+    {
+        public static void Main(String[] args)
+        {
+            string[] tokens_s = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(tokens_s[0]);
+            int t = Convert.ToInt32(tokens_s[1]);
+            string[] tokens_a = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(tokens_a[0]);
+            int b = Convert.ToInt32(tokens_a[1]);
+            string[] tokens_m = Console.ReadLine().Split(' ');
+            int m = Convert.ToInt32(tokens_m[0]);
+            int n = Convert.ToInt32(tokens_m[1]);
+            string[] apple_temp = Console.ReadLine().Split(' ');
+            int[] apple = Array.ConvertAll(apple_temp, Int32.Parse);
+            string[] orange_temp = Console.ReadLine().Split(' ');
+            int[] orange = Array.ConvertAll(orange_temp, Int32.Parse);
+        }
+    }
+}
