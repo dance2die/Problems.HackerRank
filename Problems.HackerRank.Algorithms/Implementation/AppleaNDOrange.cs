fe11c143c0246513d1f5874b7cd78740a5e355a9
dance2die <dontbothermewithspam@gmail.com>
2017-04-09 07:44:59 -0400

    "Apple and Orange" submitted successfully on first try.

--- Problems.HackerRank.Algorithms/Implementation/AppleaNDOrange.cs
+++ Problems.HackerRank.Algorithms/Implementation/AppleaNDOrange.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 
+/*
+INPUT:
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+OUTPUT:
+1
+1
+ */
 namespace Problems.HackerRank.Algorithms.Implementation
 {
     /// <summary>
@@ -20,9 +31,29 @@ namespace Problems.HackerRank.Algorithms.Implementation
             int m = Convert.ToInt32(tokens_m[0]);
             int n = Convert.ToInt32(tokens_m[1]);
             string[] apple_temp = Console.ReadLine().Split(' ');
-            int[] apple = Array.ConvertAll(apple_temp, Int32.Parse);
+            int[] apple = Array.ConvertAll(apple_temp, int.Parse);
             string[] orange_temp = Console.ReadLine().Split(' ');
-            int[] orange = Array.ConvertAll(orange_temp, Int32.Parse);
+            int[] orange = Array.ConvertAll(orange_temp, int.Parse);
+
+            int appleCount = GetFruitCount(s, t, a, m, apple);
+            int orangeCount = GetFruitCount(s, t, b, n, orange);
+
+            Console.WriteLine(appleCount);
+            Console.WriteLine(orangeCount);
+        }
+
+        private static int GetFruitCount(int fromHouse, int toHouse, int treeAt, int fruitFallCount, int[] fruitsDroppedAt)
+        {
+            int fruitCount = 0;
+
+            foreach (int fruitDroppedAt in fruitsDroppedAt)
+            {
+                int fallLocation = treeAt + fruitDroppedAt;
+                if (fromHouse <= fallLocation && fallLocation <= toHouse)
+                    fruitCount++;
+            }
+
+            return fruitCount;
         }
     }
 }
