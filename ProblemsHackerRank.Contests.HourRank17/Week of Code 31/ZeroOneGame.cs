85e72356780ed7a6351f456142e152e584015498
dance2die <dontbothermewithspam@gmail.com>
2017-04-12 05:51:51 -0400

    "Zero-One Game" stub.

---
+++ ProblemsHackerRank.Contests.HourRank17/Week of Code 31/ZeroOneGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+INPUT:
+3
+4
+1 0 0 1
+5
+1 0 1 0 1
+6
+0 0 0 0 0 0
+OUTPUT:
+Bob
+Alice
+Bob
+ */
+namespace ProblemsHackerRank.Contests.Week_of_Code_31
+{
+    /// <summary>
+    /// Zero-One Game
+    /// https://www.hackerrank.com/contests/w31/challenges/zero-one-game
+    /// </summary>
+    public class ZeroOneGame
+    {
+        public static void Main(String[] args)
+        {
+            int gameCount = Convert.ToInt32(Console.ReadLine());
+            List<int[]> games = GetTestCases(gameCount).ToList();
+
+            List<string> winners = GetWinners(games).ToList();
+            winners.ForEach(Console.WriteLine);
+        }
+
+        private static IEnumerable<string> GetWinners(List<int[]> games)
+        {
+            foreach (int[] game in games)
+            {
+                yield return GetWinner(game);
+            }
+        }
+
+        private static string GetWinner(int[] game)
+        {
+            return "";
+        }
+
+        private static IEnumerable<int[]> GetTestCases(int testCaseCount)
+        {
+            for (int i = 0; i < testCaseCount; i++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                string[] sequence_temp = Console.ReadLine().Split(' ');
+                int[] sequence = Array.ConvertAll(sequence_temp, Int32.Parse);
+                yield return sequence;
+            }
+        }
+    }
+}
