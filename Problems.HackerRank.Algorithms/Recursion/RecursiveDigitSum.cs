20a21675124784ce51acf8ae80df4e33b04a297d
dance2die <sungmkim80@gmail.com>
2017-04-28 12:38:17 -0400

    3 out of 10 pass

--- Problems.HackerRank.Algorithms/Recursion/RecursiveDigitSum.cs
+++ Problems.HackerRank.Algorithms/Recursion/RecursiveDigitSum.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 /*
 INPUT:
 148 3
 OUTPUT: 3
+
+INPUT:
+593 10
+OUTPUT: 8
  */
 namespace Problems.HackerRank.Algorithms.Recursion
 {
@@ -22,19 +25,42 @@ namespace Problems.HackerRank.Algorithms.Recursion
 		    var inputs = Console.ReadLine().Split(' ');
 		    int n = int.Parse(inputs[0]);
 		    int k = int.Parse(inputs[1]);
+
+		    BigInteger p = GetComputeDigit(n, k);
+            BigInteger superDigit = GetSuperDigit(p);
+
+		    Console.WriteLine(superDigit);
 		}
 
-        /// <summary>
+	    private static BigInteger GetSuperDigit(BigInteger p)
+	    {
+	        var digits = GetDigits(p);
+	        while (digits.Count > 1)
+	        {
+	            var sum = digits.Aggregate<BigInteger, BigInteger>(0, (total, next) => total + next);
+                digits = GetDigits(sum);
+            }
+
+	        return digits[0];
+	    }
+
+	    private static BigInteger GetComputeDigit(int n, int k)
+	    {
+	        string value = string.Join("", Enumerable.Repeat(n.ToString(), k));
+	        return BigInteger.Parse(value);
+	    }
+
+	    /// <summary>
         /// https://www.slightedgecoder.com/2017/03/11/parsing-number-digits/
         /// </summary>
-        private static List<int> GetDigits(int val)
+        private static List<BigInteger> GetDigits(BigInteger val)
         {
-            Stack<int> stack = new Stack<int>();
+            var stack = new Stack<BigInteger>();
 
-            int number = val;
+            BigInteger number = val;
             while (number > 0)
             {
-                var digit = number % 10;
+                BigInteger digit = number % 10;
                 stack.Push(digit);
 
                 number /= 10;
