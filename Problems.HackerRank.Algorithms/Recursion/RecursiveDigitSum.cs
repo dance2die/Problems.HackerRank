8124a4a81528dc43160a5a169bc54d0695f6c580
dance2die <dontbothermewithspam@gmail.com>
2017-04-28 07:20:00 -0400

    "Recursive Digit Sum" stub.

---
+++ Problems.HackerRank.Algorithms/Recursion/RecursiveDigitSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+INPUT:
+148 3
+OUTPUT: 3
+ */
+namespace Problems.HackerRank.Algorithms.Recursion
+{
+	/// <summary>
+	/// Recursive Digit Sum
+	/// https://www.hackerrank.com/challenges/recursive-digit-sum
+	/// </summary>
+	public class RecursiveDigitSum
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
