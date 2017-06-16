9c21366a9fb9e7668742b23686a1824376848698
dance2die <sungmkim80@gmail.com>
2017-06-16 13:41:14 -0400

    Pattern count priliminary tests pass

---
+++ ProblemsHackerRank.Contests.HourRank17/Week of Code 33/PatternCount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+/*
+INPUT:
+3
+100001abc101
+1001ab010abc01001
+1001010001
+OUTPUT:
+2
+2
+3
+ */
+namespace ProblemsHackerRank.Contests.Week_of_Code_33
+{
+	public class PatternCount
+	{
+		public static void Main(String[] args)
+		{
+			int q = Convert.ToInt32(Console.ReadLine());
+
+			List<int> counts = new List<int>(q);
+			for (int a0 = 0; a0 < q; a0++)
+			{
+				string s = Console.ReadLine();
+				counts.Add(GetPatternCount(s));
+			}
+
+			counts.ForEach(Console.WriteLine);
+		}
+
+		public static int GetPatternCount(string text)
+		{
+			// Using RegEx Look Behind.
+			const string pattern = @"(?<=1)(0+)1";
+			return Regex.Matches(text, pattern, RegexOptions.Singleline).Count;
+		}
+	}
+}
